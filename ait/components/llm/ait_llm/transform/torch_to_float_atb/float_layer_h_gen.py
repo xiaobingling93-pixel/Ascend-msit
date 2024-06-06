a80ce70caa65eee1b912b09c82ff31974b31556e
王国伟 <leondgarse@163.com>
2024-06-06 03:27:20 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- ait/components/llm/ait_llm/transform/torch_to_float_atb/float_layer_h_gen.py
+++ ait/components/llm/ait_llm/transform/torch_to_float_atb/float_layer_h_gen.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024-2024 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import time
 from ait_llm.transform.torch_to_float_atb import utils
@@ -5,7 +19,7 @@ from ait_llm.transform.model_parser import parser
 
 def float_layer_h_gen(model, save_name=None, save_dir=None):
     # NotImplemented
-    rr = ""
+    rr, model_name_lower = "", ""
 
     save_name = utils.init_save_name(save_name) + ".h"
     save_dir = utils.init_save_dir(model_name_lower if save_dir is None else save_dir, sub_dir="layer")
