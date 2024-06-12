f3d328201dcdaff3540d850198bddfc3306c6a28
王国伟 <leondgarse@163.com>
2024-06-12 02:30:20 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- ait/components/llm/ait_llm/transform/torch_to_float_atb/transform_float.py
+++ ait/components/llm/ait_llm/transform/torch_to_float_atb/transform_float.py
@@ -5,13 +5,14 @@
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
-#!6187
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import json
 from ait_llm.common.log import logger
 from ait_llm.transform.model_parser import parser
