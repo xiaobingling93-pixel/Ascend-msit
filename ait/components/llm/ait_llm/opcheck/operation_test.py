bf4700cca41f5d8aa44b68d2cdc98985ad83a474
xuchuan18 <xuchuan182024@163.com>
2024-06-18 15:40:22 +0800

    修改bug

--- ait/components/llm/ait_llm/opcheck/operation_test.py
+++ ait/components/llm/ait_llm/opcheck/operation_test.py
@@ -175,12 +175,13 @@ class OperationTest(unittest.TestCase):
         logger_text = f"———————— {self.op_id} {self.op_name} test start ————————"
         logger.info(logger_text)
         if self.atb_rerun:
-            out_tensors = self.rerun_op(execute_type)
             if self.op_name in ("AllGatherOperation", "AllReduceOperation", "LinearParallelOperation"):
                 logger_text = f"{self.op_name} needs data on all ranks and atb-rerun is unsupported. \
                                 The dump data will be used for comparison."
                 logger.warning(logger_text)
                 out_tensors = self.out_tensors
+            else:
+                out_tensors = self.rerun_op(execute_type)
         else:
             out_tensors = self.out_tensors
 
