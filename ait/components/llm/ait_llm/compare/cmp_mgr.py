a85480955069d5f8011db594c2910c08678cc6cc
mjlovesz <lcj200202513@163.com>
2024-06-19 18:14:52 +0800

    修复存在 warmup 场景，自动比对的问题

--- ait/components/llm/ait_llm/compare/cmp_mgr.py
+++ ait/components/llm/ait_llm/compare/cmp_mgr.py
@@ -67,9 +67,10 @@ class CompareMgr:
                 self.compare_token(token_id, token_id, op_map)
                 logger.debug("compared tokens is %s ", str(token_id))
         else:
-            if len(golden_tokens) == 1 and len(my_tokens) == 1:
+            if self.golden_data.get_token_id() is not None and self.my_data.get_token_id() is not None:
                 # 指定token，强制比对
-                self.compare_token(golden_tokens[0], my_tokens[0], op_map)
+                for golden_token, my_token in itertools.product(golden_tokens, my_tokens):
+                    self.compare_token(golden_token, my_token, op_map)
             else:
                 logger.error(
                     f"my tokens is {my_tokens} and golden tokens is {golden_tokens}. The two cannot be matched."
@@ -84,24 +85,24 @@ class CompareMgr:
             # 获取到所有需要比较的 tensor 的路径
             golden_tensor_paths = list(self.golden_data.get_tensor_path(golden_token_id, golden_op, golden_op_location))
             my_tensor_paths = list(self.my_data.get_tensor_path(my_token_id, my_op, my_op_location))
-            
+
             if len(golden_tensor_paths) == len(my_tensor_paths):
                 golden_tensor_paths.sort()
                 my_tensor_paths.sort()
                 tensor_pairs = zip(golden_tensor_paths, my_tensor_paths)
             else:
                 tensor_pairs = itertools.product(golden_tensor_paths, my_tensor_paths)
-                
+
             # 交叉比对，记录结果
             for golden_tensor_path, my_tensor_path in tensor_pairs:
                 logger.debug("golden_path: %s; my_path:%s", str(golden_tensor_path), str(my_tensor_path))
 
-                # 1. get tensor_datas
+                # 1. get tensor_datas 多卡数据合并
                 _, my_tensor_datas = get_multi_tensor_paths(
-                    self.golden_data.get_token_path(), my_tensor_path, tensor_sub_dir=""
+                    self.golden_data.get_token_path(golden_token_id), my_tensor_path, tensor_sub_dir=""
                 )
                 _, golden_tensor_datas = get_multi_tensor_paths(
-                    self.golden_data.get_token_path(), golden_tensor_path, tensor_sub_dir=""
+                    self.golden_data.get_token_path(my_token_id), golden_tensor_path, tensor_sub_dir=""
                 )
                 # 2. concat tensor_datas
                 dim_atb = get_cat_dim(my_tensor_datas, golden_tensor_datas)
