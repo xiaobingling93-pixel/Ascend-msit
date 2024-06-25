89627a2fa8929c269e1e821ede0d4ef52f7107ea
王国伟 <leondgarse@163.com>
2024-06-25 05:42:46 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

---
+++ ait/components/benchmark/tests/test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Copyright (c) 2023-2023 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CUR_PATH=$(dirname $(readlink -f $0))
+COMPONENTS_PATH=$CUR_PATH/../../../
+SOURCE_CODE_PATH=$COMPONENTS_PATH/components/benchmark
+echo "CUR_PATH=$CUR_PATH, COMPONENTS_PATH=$COMPONENTS_PATH, SOURCE_CODE_PATH=$SOURCE_CODE_PATH"
+
+if [ -f "../requirements.txt" ]; then
+    pip3 install -r ../requirements.txt
+fi
+
+if [ -f "$CUR_PATH/resources" ]; then
+    chmod -R 750 $CUR_PATH/resources
+fi
+
+PYTHONPATH=$COMPONENTS_PATH:$PYTHONPATH coverage run --source $SOURCE_CODE_PATH coverage run -m pytest -vv $CUR_PATH --disable-warnings
+
+RETURN_CODE=0
+if [ $? == 0 ]; then
+    coverage combine $CUR_PATH
+    coverage report -m --omit="test_*.py" -i > $CUR_PATH/test.coverage
+    coverage_rate=$(awk '/TOTAL/{print $4}' $CUR_PATH/test.coverage | cut -d '%' -f 1)
+    echo "coverage_rate=$coverage_rate%"
+
+    coverage_target=50  # Current is only 51%
+    if [[ "$coverage_rate" -ne "" && "$coverage_rate" -lt "$target" ]]; then
+        echo "coverage rate too low(<${coverage_target}%), currently reaches only ${coverage_rate}%."
+        RETURN_CODE=1
+    fi
+else
+    echo "coverage run failed! "
+    RETURN_CODE=1
+fi
+
+unlink $COMPONENTS_PATH
+exit $RETURN_CODE
---
+++ ait/components/benchmark/tests/test_args.py
@@ -0,0 +1,130 @@
+# Copyright (c) 2023-2023 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import argparse
+
+import pytest
+from msit_benchmark.__main__ import get_cmd_instance
+
+
+CUR_DIR = f"{os.path.dirname(__file__)}/"
+FAKE_OM_PATH="test_args_fake_model.om"
+FAKE_BIN_PATH="test_args_fake_data.bin"
+FAKE_ACL_JSON_PATH="test_args_fake_acl.json"
+FAKE_AIPP_CFG_PATH="test_args_fake_aipp.cfg"
+INVALID_ARG = "invalid_arg"
+
+FULL_CMD_DICT = {
+    "--om-model": FAKE_OM_PATH,
+    "--input": FAKE_BIN_PATH,
+    "--output": "output/",
+    "--output-dirname": "outdir/",
+    "--outfmt": "NPY",
+    "--loop": "100",
+    "--debug": "0",
+    "--device": "0,1",
+    "--dym-batch": "16",
+    "--dym-hw": "224,224",
+    "--dym-dims": "1,3,224,224",
+    "--dym-shape": "1,3,224,224",
+    "--output-size": "10000",
+    "--auto-set-dymshape-mode": "0",
+    "--auto-set-dymdims-mode": "0",
+    "--batch-size": "16",
+    "--pure-data-type": "zero",
+    "--profiler": "0",
+    "--dump": "0",
+    "--acl-json-path": FAKE_ACL_JSON_PATH,
+    "--output-batchsize-axis": "1",
+    "--run-mode": "array",
+    "--display-all-summary": "0",
+    "--warmup-count": "1",
+    "--dym-shape-range": "1~3,3,224,224-226",
+    "--aipp-config": FAKE_AIPP_CFG_PATH,
+    "--energy-consumption": "0",
+    "--npu-id": "0",
+    "--backend": "trtexec",
+    "--perf": "0",
+    "--pipeline": "0",
+    "--profiler-rename": "0",
+    "--dump-npy": "0",
+    "--divide-input": "0",
+    "--threads": "1",
+}
+
+SHORT_CMD_DICT = {
+    "-om": FAKE_OM_PATH,
+    "-i": FAKE_BIN_PATH,
+    "-o": "output/",
+    "-od": "outdir/",
+    "--outfmt": "NPY",
+    "--loop": "100",
+    "--debug": "0",
+    "-d": "0,1",
+    "-db": "16",
+    "-dhw": "224,224",
+    "-dd": "1,3,224,224",
+    "-ds": "1,3,224,224",
+    "-outsize": "10000",
+    "-asdsm": "0",
+    "-asddm": "0",
+    "--batch-size": "16",
+    "-pdt": "zero",
+    "-pf": "0",
+    "--dump": "0",
+    "-acl": FAKE_ACL_JSON_PATH,
+    "-oba": "1",
+    "-rm": "array",
+    "-das": "0",
+    "-wcount": "1",
+    "-dr": "1~3,3,224,224-226",
+    "-aipp": FAKE_AIPP_CFG_PATH,
+    "-ec": "0",
+    "--npu-id": "0",
+    "--backend": "trtexec",
+    "--perf": "0",
+    "--pipeline": "0",
+    "--profiler-rename": "0",
+    "--dump-npy": "0",
+    "--divide-input": "0",
+    "--threads": "1",
+}
+
+def benchmark_argparse(argv):
+    aa = get_cmd_instance()
+    parser = argparse.ArgumentParser()
+    aa.add_arguments(parser)
+    args_parser = parser.parse_args(argv)
+    return args_parser
+
+
+def call_benchmark_cmd(argv):
+    aa = get_cmd_instance()
+    args = benchmark_argparse(argv)
+    return aa.handle(args)
+
+
+def cmd_dict_to_list(cmd_dict):
+    cmd_list = []
+    for key, value in cmd_dict.items():
+        cmd_list.append(key)
+        cmd_list.append(value)
+    return cmd_list
+
+def test_benchmark_argparse_given_valid_when_full_then_pass():
+    benchmark_argparse(cmd_dict_to_list(FULL_CMD_DICT))
+
+def test_app_analyze_given_opencv_csv_when_short_then_pass():
+    benchmark_argparse(cmd_dict_to_list(SHORT_CMD_DICT))
