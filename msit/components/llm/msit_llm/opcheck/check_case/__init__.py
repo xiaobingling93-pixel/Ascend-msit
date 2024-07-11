e596d451eb42ac857542c99ae9198aa39df7dd3f
xuchuan18 <xuchuan182024@163.com>
2024-07-11 15:47:46 +0800

    补充算子

--- msit/components/llm/msit_llm/opcheck/check_case/__init__.py
+++ msit/components/llm/msit_llm/opcheck/check_case/__init__.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from msit_llm.opcheck.check_case.gating import OpcheckGatingOperation
+from msit_llm.opcheck.check_case.index_add import OpcheckIndexAddOperation
+from msit_llm.opcheck.check_case.nonzero import OpcheckNonzeroOperation
+from msit_llm.opcheck.check_case.onehot import OpcheckOnehotOperation
 from msit_llm.opcheck.check_case.activation import OpcheckActivationOperation
 from msit_llm.opcheck.check_case.all_gather import OpcheckAllGatherOperation
 from msit_llm.opcheck.check_case.all_reduce import OpcheckAllReduceOperation
@@ -19,21 +23,16 @@ from msit_llm.opcheck.check_case.broadcast import OpcheckBroadcastOperation
 from msit_llm.opcheck.check_case.concat import OpcheckConcatOperation
 from msit_llm.opcheck.check_case.cumsum import OpcheckCumsumOperation
 from msit_llm.opcheck.check_case.elewise import OpcheckElewiseAddOperation
-from msit_llm.opcheck.check_case.fastsoftmax import OpcheckFastSoftMaxOperation
-from msit_llm.opcheck.check_case.fastsoftmaxgrad import OpcheckFastSoftMaxGradOperation
 from msit_llm.opcheck.check_case.fill import OpcheckFillOperation
 from msit_llm.opcheck.check_case.gather import OpcheckGatherOperation
-from msit_llm.opcheck.check_case.genattentionmask import OpcheckElewiseSubOperation
 from msit_llm.opcheck.check_case.kv_cache import OpcheckKvCacheOperation
 from msit_llm.opcheck.check_case.linear import OpcheckLinearOperation
 from msit_llm.opcheck.check_case.linear_sparse import OpcheckLinearSparseOperation
-from msit_llm.opcheck.check_case.matmul import OpcheckMatmulOperation
 from msit_llm.opcheck.check_case.pad import OpcheckPadOperation
 from msit_llm.opcheck.check_case.paged_attention import OpcheckPagedAttentionAttentionOperation
 from msit_llm.opcheck.check_case.repeat import OpcheckRepeatOperation
 from msit_llm.opcheck.check_case.reshape_and_cache import OpcheckReshapeAndCacheOperation
 from msit_llm.opcheck.check_case.rms_norm import OpcheckRmsNormOperation
-from msit_llm.opcheck.check_case.rope_grad import OpcheckRopeGradOperation
 from msit_llm.opcheck.check_case.rope import OpcheckUnpadRopeOperation
 from msit_llm.opcheck.check_case.self_attention import OpcheckUnpadSelfAttentionOperation
 from msit_llm.opcheck.check_case.set_value import OpcheckSetValueOperation
@@ -41,7 +40,6 @@ from msit_llm.opcheck.check_case.slice import OpcheckSliceOperation
 from msit_llm.opcheck.check_case.softmax import OpcheckSoftmaxOperation
 from msit_llm.opcheck.check_case.sort import OpcheckSortOperation
 from msit_llm.opcheck.check_case.split import OpcheckAddOperation
-from msit_llm.opcheck.check_case.stridebatchmatmul import OpcheckStridedBatchMatmulOperation
 from msit_llm.opcheck.check_case.topk_topp_sampling import OpcheckToppOperation
 from msit_llm.opcheck.check_case.transpose import OpcheckTransposeOperation
 from msit_llm.opcheck.check_case.unpad import OpcheckUnpadOperation
@@ -52,9 +50,19 @@ from msit_llm.opcheck.check_case.multinomial import OpcheckMultinomialOperation
 from msit_llm.opcheck.check_case.reduce import OpcheckReduceOperation
 from msit_llm.opcheck.check_case.transdata import OpcheckTransdataOperation
 from msit_llm.opcheck.check_case.where import OpcheckWhereOperation
+# 已删除算子，后续不再维护
+from msit_llm.opcheck.check_case.fastsoftmax import OpcheckFastSoftMaxOperation
+from msit_llm.opcheck.check_case.fastsoftmaxgrad import OpcheckFastSoftMaxGradOperation
+from msit_llm.opcheck.check_case.genattentionmask import OpcheckElewiseSubOperation
+from msit_llm.opcheck.check_case.matmul import OpcheckMatmulOperation
+from msit_llm.opcheck.check_case.rope_grad import OpcheckRopeGradOperation
+from msit_llm.opcheck.check_case.stridebatchmatmul import OpcheckStridedBatchMatmulOperation
 
-
-OP_NAME_DICT = dict({
+ = dict({
+    "GatingOperation":OpcheckGatingOperation,
+    "IndexAddOperation":OpcheckIndexAddOperation,
+    "NonzeroOperation":OpcheckNonzeroOperation,
+    "OnehotOperation":OpcheckOnehotOperation,
     "ActivationOperation":OpcheckActivationOperation,
     "AllGatherOperation":OpcheckAllGatherOperation,
     "AllReduceOperation":OpcheckAllReduceOperation,
@@ -62,29 +70,23 @@ OP_NAME_DICT = dict({
     "ConcatOperation":OpcheckConcatOperation,
     "CumsumOperation":OpcheckCumsumOperation,
     "ElewiseOperation":OpcheckElewiseAddOperation,
-    "FastSoftMaxOperation":OpcheckFastSoftMaxOperation,
-    "FastSoftMaxGradOperation":OpcheckFastSoftMaxGradOperation,
     "FillOperation":OpcheckFillOperation,
     "GatherOperation":OpcheckGatherOperation,
-    "GenAttentionMaskOperation":OpcheckElewiseSubOperation,
     "KvCacheOperation":OpcheckKvCacheOperation,
     "LinearOperation":OpcheckLinearOperation,
     "LinearSparseOperation":OpcheckLinearSparseOperation,
-    "MatmulOperation":OpcheckMatmulOperation,
     "PadOperation":OpcheckPadOperation,
     "PagedAttentionOperation":OpcheckPagedAttentionAttentionOperation,
     "RepeatOperation":OpcheckRepeatOperation,
     "ReshapeAndCacheOperation":OpcheckReshapeAndCacheOperation,
     "RmsNormOperation":OpcheckRmsNormOperation,
     "RopeOperation":OpcheckUnpadRopeOperation,
-    "RopeGradOperation":OpcheckRopeGradOperation,
     "SelfAttentionOperation":OpcheckUnpadSelfAttentionOperation,
     "SetValueOperation":OpcheckSetValueOperation,
     "SliceOperation":OpcheckSliceOperation,
     "SoftmaxOperation":OpcheckSoftmaxOperation,
     "SortOperation":OpcheckSortOperation,
     "SplitOperation":OpcheckAddOperation,
-    "StridedBatchMatmulOperation":OpcheckStridedBatchMatmulOperation,
     "TopkToppSamplingOperation":OpcheckToppOperation,
     "TransposeOperation":OpcheckTransposeOperation,
     "UnpadOperation":OpcheckUnpadOperation,
@@ -95,4 +97,11 @@ OP_NAME_DICT = dict({
     "ReduceOperation":OpcheckReduceOperation,
     "TransdataOperation":OpcheckTransdataOperation,
     "WhereOperation":OpcheckWhereOperation,
+    # 已删除算子，后续不再维护
+    "FastSoftMaxOperation":OpcheckFastSoftMaxOperation,
+    "FastSoftMaxGradOperation":OpcheckFastSoftMaxGradOperation,
+    "GenAttentionMaskOperation":OpcheckElewiseSubOperation,
+    "MatmulOperation":OpcheckMatmulOperation,
+    "RopeGradOperation":OpcheckRopeGradOperation,
+    "StridedBatchMatmulOperation":OpcheckStridedBatchMatmulOperation,
 })
