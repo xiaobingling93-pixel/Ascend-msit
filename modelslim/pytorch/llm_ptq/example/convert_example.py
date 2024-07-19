583068f0010754371f38089d1d74fcd79909413f
孔炳翔 <1120200577@qq.com>
2024-07-19 16:48:49 +0800

    review

--- modelslim/pytorch/llm_ptq/example/convert_example.py
+++ modelslim/pytorch/llm_ptq/example/convert_example.py
@@ -1,13 +1,14 @@
 # Copyright Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
 import json
 import os
+import stat
 
 import numpy as np
 from safetensors.torch import save_file
 import torch
 
 
-def int4_to_int8_forGLM(i4w):
+def int4_to_int8_forchatglm(i4w):
     """
     ChatGLM2-6B开源int4量化权重的保存方式，会将两个int4合并成一个int8进行保存，具体实现逻辑见开源代码中quantization.py文件
     此处通过python实现了对应逻辑
@@ -17,11 +18,11 @@ def int4_to_int8_forGLM(i4w):
     weight0 = weight[:, :1] << 4
     weight1 = weight[:, 1:] & 0b00001111
     i8w = weight0 | weight1
-    i8w = i8w.reshape(n, k//2).to(torch.int8)
+    i8w = i8w.reshape(n, k // 2).to(torch.int8)
     return i8w
 
 
-def int8_to_int4_forGLM(i8w):
+def int8_to_int4_forchatglm(i8w):
     """
     仿照开源将一个int8tensor分离解码成两个int4
     """
@@ -50,8 +51,8 @@ class MSModelSlimWeightProcessor:
         self.modelslim_weight_dict = weight
         self.modelslim_description_json = {}
         self.num_layers = 28  # 示例量化权重DecoderLayer的层数，请根据实际修改
-        self.QuantType = 'W8A16'  # 示例量化权重的量化类型，请根据实际修改
-        self.modelslim_description_json['model_quant_type'] = self.QuantType
+        self.quant_type = 'W8A16'  # 示例量化权重的量化类型，请根据实际修改
+        self.modelslim_description_json['model_quant_type'] = self.quant_type
         for key in self.modelslim_weight_dict.keys():
             self.modelslim_description_json[key] = 'FLOAT'
     
@@ -74,18 +75,18 @@ class MSModelSlimWeightProcessor:
             # 示例量化权重的weight的名称和msmodelslim的一致，名称不需要修改
             # 示例量化权重，会将两个int4权重保存成一个int8，此处需要将一个权重解码成两个int4的权重
             self.modelslim_weight_dict[weight_key] = int8_to_int4_forGLM(self.modelslim_weight_dict[weight_key])
-            self.modelslim_description_json[weight_key] = self.QuantType
+            self.modelslim_description_json[weight_key] = self.quant_type
 
             # 示例量化权重的weight_scale名称和msmodelslim的一致，名称不需要修改
             # 示例量化权重的weight_scale的shape是[n]，msmodelslim的weight_scale的shape是[n,1]
             self.modelslim_weight_dict[weight_scale_key] = \
                 self.modelslim_weight_dict[weight_scale_key].unsqueeze(dim=1).to(torch.float16)
-            self.modelslim_description_json[weight_scale_key] = self.QuantType
+            self.modelslim_description_json[weight_scale_key] = self.quant_type
 
             # 示例量化权重为对称量化，不存在weight_offset，msmodelslim工具对称量化会生成全0的tensor
             self.modelslim_weight_dict[weight_offset_key] = \
                 torch.zeros(self.modelslim_weight_dict[weight_scale_key].shape).to(torch.float16)
-            self.modelslim_description_json[weight_offset_key] = self.QuantType
+            self.modelslim_description_json[weight_offset_key] = self.quant_type
 
     # 处理示量化权重中Linear层的权重量化参数，对应msmodelslim支持的量化类型W8A8和W8A8S
     def weight_activation_process(self):
@@ -133,11 +134,11 @@ class MSModelSlimWeightProcessor:
                  self.modelslim_description_json[ori_input_offset_key],
                  self.modelslim_description_json[ori_deq_scale_key],
                  self.modelslim_description_json[ori_quant_bias_key])
-            self.modelslim_description_json[msmodelslim_weight_key] = self.QuantType
-            self.modelslim_description_json[msmodelslim_input_scale_key] = self.QuantType
-            self.modelslim_description_json[msmodelslim_input_offset_key] = self.QuantType
-            self.modelslim_description_json[msmodelslim_deq_scale_key] = self.QuantType
-            self.modelslim_description_json[msmodelslim_quant_bias_key] = self.QuantType
+            self.modelslim_description_json[msmodelslim_weight_key] = self.quant_type
+            self.modelslim_description_json[msmodelslim_input_scale_key] = self.quant_type
+            self.modelslim_description_json[msmodelslim_input_offset_key] = self.quant_type
+            self.modelslim_description_json[msmodelslim_deq_scale_key] = self.quant_type
+            self.modelslim_description_json[msmodelslim_quant_bias_key] = self.quant_type
 
     # 对于smooth quant算法，norm层权重更换示例
     def anti_outlier_process(self):
@@ -164,8 +165,8 @@ class MSModelSlimWeightProcessor:
             # 删除modelslim_weight_dict中已被改名的重复权重，并修改描述文件，请根据实际修改
             del self.modelslim_weight_dict[ori_norm_weight_key], self.modelslim_weight_dict[ori_norm_bias_key]
             del self.modelslim_description_json[ori_norm_weight_key], self.modelslim_description_json[ori_norm_bias_key]
-            self.modelslim_description_json[msmodelslim_norm_module_weight_key] = self.QuantType
-            self.modelslim_description_json[msmodelslim_norm_module_bias_key] = self.QuantType
+            self.modelslim_description_json[msmodelslim_norm_module_weight_key] = self.quant_type
+            self.modelslim_description_json[msmodelslim_norm_module_bias_key] = self.quant_type
 
             # 若开源权重不涉及回退操作，此处传入None即可
             self.modelslim_weight_dict[msmodelslim_norm_weight_key] = None
@@ -174,7 +175,7 @@ class MSModelSlimWeightProcessor:
     # kv cache量化
     def kv_cache_process(self):
         # 在描述文件中添加kv_cache量化的说明
-        self.modelslim_description_json["kv_cache_type": "C8"]
+        self.modelslim_description_json["kv_cache_type"] = "C8"
         # 获取开源量化权重中，k_proj和v_proj层的名称
         kv_linear_list = []
         for i in range(self.num_layers):
@@ -196,18 +197,19 @@ class MSModelSlimWeightProcessor:
             del (self.modelslim_description_json[ori_kvcache_scale_key], 
                  self.modelslim_description_json[ori_kvcache_offset_key])
 
-            self.modelslim_description_json[msmodelslim_kvcaceh_scale_key] = self.QuantType
-            self.modelslim_description_json[msmodelslim_kvcaceh_offset_key] = self.QuantType
+            self.modelslim_description_json[msmodelslim_kvcaceh_scale_key] = self.quant_type
+            self.modelslim_description_json[msmodelslim_kvcaceh_offset_key] = self.quant_type
 
     # 储存量化权重和描述文件
     def save(self, path):
         if not os.path.exists(path):
             os.mkdir(path)
-        safetensors_name = f"quant_model_weight_{self.QuantType.lower()}.safetensors"
-        json_name = f"quant_model_description_{self.QuantType.lower()}.json"
+        safetensors_name = f"quant_model_weight_{self.quant_type.lower()}.safetensors"
+        json_name = f"quant_model_description_{self.quant_type.lower()}.json"
         safetensors_path = os.path.join(path, safetensors_name)
         json_path = os.path.join(path, json_name)
         save_file(self.modelslim_weight_dict, safetensors_path)
+        default_save_mode = stat.S_IWUSR | stat.IRUSR # 600
         with os.fdopen(os.open(json_path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC), 'w') as json_file:
             json.dump(self.modelslim_description_json, json_file, indent=2)
 
