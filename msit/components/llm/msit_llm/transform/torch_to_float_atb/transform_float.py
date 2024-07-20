727e2d14ecd9aef143f0b223d46313b8820f6604
tong-bowen <tongbowen1996@163.com>
2024-07-20 14:23:57 +0800

    clean code

--- msit/components/llm/msit_llm/transform/torch_to_float_atb/transform_float.py
+++ msit/components/llm/msit_llm/transform/torch_to_float_atb/transform_float.py
@@ -15,7 +15,7 @@
 import os
 import json
 from msit_llm.common.log import logger
-from msit_llm.transform.model_parser import kind
+from msit_llm.transform.model_parser import kind, parser
 from utils import get_repeat_box_layer, dag_to_model
 import torch.nn as nn
 
