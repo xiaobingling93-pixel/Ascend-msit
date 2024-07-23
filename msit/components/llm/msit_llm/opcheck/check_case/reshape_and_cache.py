7ab527a4c6896c68b3bd5128125e2d58bf19a8d4
xuchuan18 <xuchuan182024@163.com>
2024-07-23 14:32:40 +0800

    reshape_and_cache修改中

--- msit/components/llm/msit_llm/opcheck/check_case/reshape_and_cache.py
+++ msit/components/llm/msit_llm/opcheck/check_case/reshape_and_cache.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from enum import Enum
 import torch
 import torch_npu
 
@@ -19,8 +20,27 @@ from msit_llm.opcheck import operation_test
 from msit_llm.common.log import logger
 
 
+class CompressType(Enum):
+    COMPRESS_TYPE_UNDEFINED = 0
+    COMPRESS_TYPE_KVHEAD = 1
+
+
 class OpcheckReshapeAndCacheOperation(operation_test.OperationTest):
     def golden_calc(self, in_tensors):
+        num_tokens, num_heads, head_size = in_tensors[0].shape
+        data_type = in_tensors[0].dtype
+        slot_mapping = in_tensors[4]
+        soc_version = self.get_soc_version()
+        if soc_version == "Ascend910B":
+            num_blocks, block_size, _, _ = in_tensors[2].shape
+            key_expect = in_tensors[2]
+            value_expect = in_tensors[3]
+            compress_type = self.op_param.get("compressType", CompressType.COMPRESS_TYPE_UNDEFINED)
+            if compress_type == CompressType.COMPRESS_TYPE_KVHEAD:
+                pass
+        else:
+            pass
+            
         golden = []
         inplace_idx = self.case_info.get("inplace_idx", None)
         for index in inplace_idx:
