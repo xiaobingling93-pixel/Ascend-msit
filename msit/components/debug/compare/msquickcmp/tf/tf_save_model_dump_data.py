b22b72aecf6c0b068fb14522d00faa276ddf23ed
yuliangbin <y1416490440@163.com>
2024-07-24 21:08:43 +0800

    Revert "[debug-compare]支持tf2.6 save_model的dump、compare"
    This reverts commit 83c7dcf291093cb8fe2814bfe5e2dd0ccce3fe78.

--- msit/components/debug/compare/msquickcmp/tf/tf_save_model_dump_data.py
+++ msit/components/debug/compare/msquickcmp/tf/tf_save_model_dump_data.py
@@ -25,7 +25,6 @@ import tfdbg_ascend as tfdbg
 from msquickcmp.common import utils, tf_common
 from msquickcmp.common.dump_data import DumpData
 
-
 class TfSaveModelDumpData(DumpData):
     """
     This class is used to generate GUP dump data of the tf2.6 save_model.
@@ -68,23 +67,21 @@ class TfSaveModelDumpData(DumpData):
         Generate tf2.6 save_model dump data
         :return tf2.6 save_model dump data directory
         """
-        with UmaskWrapper():
-            try:
-                model = tf.saved_model.load(self.model_path)
-            except Exception as e:
-                raise RuntimeError(
-                    f"{self.model_path} saved_model load error, please check that the saved_model is the "
-                    f"correct file ") from e
-            # enable the dump function
-            tfdbg.enable()
-            # change the current directory to the specified directory
-            current_dir = os.getcwd()
-            os.chdir(self.dump_data_tf)
-            model(self.inputs_data)
-            # model.predict(self.inputs_data)
-            os.chdir(current_dir)
-            self._rename_ops()
-            return self.dump_data_tf
+        try:
+            model = tf.saved_model.load(self.model_path)
+        except Exception as e:
+            raise RuntimeError(f"{self.model_path} saved_model load error, please check that the saved_model is the "
+                               f"correct file ") from e
+        # enable the dump function
+        tfdbg.enable()
+        # change the current directory to the specified directory
+        current_dir = os.getcwd()
+        os.chdir(self.dump_data_tf)
+        model(self.inputs_data)
+        # model.predict(self.inputs_data)
+        os.chdir(current_dir)
+        self._rename_ops()
+        return self.dump_data_tf
 
     def _rename_ops(self):
         ops_dump_data_dir = self.dump_data_tf
@@ -112,14 +109,3 @@ class TfSaveModelDumpData(DumpData):
                 f"but found version {current_version}. Please install the correct "
                 "version of TensorFlow."
             )
-
-
-class UmaskWrapper:
-    def __init__(self, umask=0o027):
-        self.umask, self.ori_umask = umask, None
-
-    def __enter__(self):
-        self.ori_umask = os.umask(self.umask)
-
-    def __exit__(self, exc_type=None, exc_value=None, exc_tb=None):
-        os.umask(self.ori_umask)
