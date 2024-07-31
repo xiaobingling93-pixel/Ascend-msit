991482515c9605cb5c4e1c10c69c5383fdad7b3a
xuchuan18 <xuchuan182024@163.com>
2024-07-31 09:38:04 +0800

    self attention修改中

--- msit/components/llm/msit_llm/opcheck/check_case/self_attention.py
+++ msit/components/llm/msit_llm/opcheck/check_case/self_attention.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from enum import Enum
 import json
 import math
 import torch
@@ -22,7 +23,78 @@ from msit_llm.opcheck import operation_test
 from msit_llm.common.log import logger
 
 
+class CalcType(Enum):
+    UNDEFINED = 0 # decoder&encoder for flashAttention
+    ENCODER = 1 # encoder for flashAttention
+    DECODER = 2 # decoder for flashAttention
+    PA_ENCODER = 3 # encoder for pagedAttention
+
+
+class KvCacheCfg(Enum):
+    K_CACHE_V_CACHE = 0 # 默认值，进行kvcache处理
+    K_BYPASS_V_BYPASS = 1 # 直接传入kvcache
+
+
+class MaskType(Enum):
+    MASK_TYPE_UNDEFINED = 0
+    MASK_TYPE_NROM = 1
+    MASK_TYPE_ALIBI = 2
+    MASK_TYPE_NORM_COMPRESS = 3
+    MASK_TYPE_ALIBI_COMPRESS = 4
+    MASK_TYPE_ALIBI_COMPRESS_SQRT = 5
+    MASK_TYPE_ALIBI_COMPRESS_LEFT_ALIGN = 6
+
+
+class KernelType(Enum):
+    KERNELTYPE_DEFAULT = 0
+    KERNELTYPE_HIGH_PRECISION = 1
+
+
 class OpcheckUnpadSelfAttentionOperation(operation_test.OperationTest):
+    @staticmethod
+    def get_asdops_param(in_tensors, op_param):
+        q, k, v, mask, seq_len = in_tensors
+        asdops_param = {}
+        asdops_param["head_num"] = op_param.get("headNum", 0)
+        asdops_param["is_decoder"] = False
+        asdops_param["embeddim"] = int(q.shape[1] / asdops_param["head_num"])
+        asdops_param["kv_head"] = op_param.get("kvHeadNum", 0)
+        asdops_param["is_mask"] = op_param.get("MaskType", MaskType.MASK_TYPE_UNDEFINED) != MaskType.MASK_TYPE_UNDEFINED
+        asdops_param["qk_scale"] = op_param.get("qkScale", 1.0)
+        asdops_param["post_mask_coff"] = -3e38
+        if op_param.get("kernelType", KernelType.KERNELTYPE_DEFAULT) == KernelType.KERNELTYPE_HIGH_PRECISION:
+            asdops_param["post_mask_coff"] = 1
+        
+        asdops_param["data_type"] = q.dtype
+        asdops_param["q_ntokens"] = q.shape[0]
+        asdops_param["kv_ntokens"] = k.shape[0]
+        asdops_param["q_seqlen"] = seq_len.tolist()
+        asdops_param["maskType"] = op_param.get("MaskType", MaskType.MASK_TYPE_UNDEFINED)
+
+        MASK_TYPE_NO_MASK = 0
+        MASK_TYPE_NO_HEAD = 1
+        MASK_TYPE_NO_BATCH = 2
+        MASK_TYPE_ALIBI_WITH_BATCH = 3
+        MASK_TYPE_ALIBI_NO_BATCH = 4
+        MASK_TYPE_NO_HEAD_DECODER = 5
+
+        mask_type_dict = {
+            # 四维的alibi mask
+            MASK_TYPE_ALIBI_WITH_BATCH: (( lambda mask, idx, q_s, kv_s: (mask[idx, :, :q_s, :kv_s]))),
+            # 三维的alibi mask
+            MASK_TYPE_ALIBI_NO_BATCH: (( lambda mask, idx, q_s, kv_s: (mask[:, :q_s, :kv_s]))),
+            MASK_TYPE_NO_HEAD: (( lambda mask, idx, q_s, kv_s: (mask[idx, :q_s:, :kv_s]))),
+            MASK_TYPE_NO_HEAD_DECODER: (( lambda mask, idx, q_s, kv_s: (mask[idx, :q_s, :kv_s]))),
+            MASK_TYPE_NO_BATCH: (( lambda mask, idx, q_s, kv_s: (mask[:, :q_s:, :kv_s]))),
+            # 不加mask
+            MASK_TYPE_NO_HEAD: (( lambda mask, idx, q_s, kv_s: 0))
+        }
+
+        asdops_mask_type = 0
+        asdops_param["mask_info"] = mask_type_dict[asdops_mask_type]
+
+        return asdops_param
+
     def group_matmul(self, heads, group_num, in_a, in_b):
         try:
             group_head = heads // group_num
@@ -154,25 +226,144 @@ class OpcheckUnpadSelfAttentionOperation(operation_test.OperationTest):
 
     def pa_encoder_golden_func(self, in_tensors):
         return self.encoder_golden_func(in_tensors)
+    
+    @staticmethod
+    def reshape_qkv(qkv):
+        # kv的计算方式似乎与q不同
+        if len(qkv.shape) == 3:
+            dim0, dim1, dim2 = qkv.shape
+            qkv = qkv.contiguous().view(dim0, dim1 * dim2)
+        elif len(qkv.shape) == 4:
+            dim0, dim1, dim2, dim3 = qkv.shape
+            qkv = qkv.contiguous().view(dim0 * dim1, dim2 * dim3)
+        return qkv
 
     def golden_calc(self, in_tensors):
-        calc_type = self.op_param.get("calcType", 0)
-        if calc_type == 0:
-            logger_text = f"CalcType: {calc_type}. Undefined calcType!"
-            logger.debug(logger_text)
-            out = self.undefined_golden_func(in_tensors)
-        elif calc_type == 1:
-            logger_text = f"CalcType: {calc_type}. Using encoder of FlashAttention!"
-            logger.debug(logger_text)
-            out = self.encoder_golden_func(in_tensors)
-        elif calc_type == 2:
-            logger_text = f"CalcType: {calc_type}. Using decoder of FlashAttention!"
-            logger.debug(logger_text)
-            out = self.decoder_golden_func(in_tensors)
+        kvcache_cfg = self.op_param.get("kvcacheCfg", KvCacheCfg.K_CACHE_V_CACHE)
+        if kvcache_cfg == KvCacheCfg.K_BYPASS_V_BYPASS:
+            q_offset, k_offset, v_offset = 0, 0, 0
+            isdecoder = 1
+            is_clamp = 0
+            batch = 
+            heads = 
+            embed = 
+            max_seq = 
+            q_seqlen = in_tensors[5]
+            kv_seqlen = in_tensors[4]
+            kv_head = 
+            mask = in_tensors[3]
+            is_mask = True
+            q = in_tensors[0]
+            k = in_tensors[1]
+            v = in_tensors[2]
+            q_ntokens = sum(q_seqlen)
+            kv_ntokens = sum(kv_seqlen)
+            layer_id = in_tensors[6][0]
+            is_multi_layer = True
+            s = None
+            _p = None
+            out = None
+
+            for idx in range(batch):
+                q_s = q_seqlen[idx]
+                kv_s = kv_seqlen[idx]
+                q_slice = q[q_offset:q_offset + q_s][:].view(q_s, heads, embed).permute(1, 0, 2)
+                k_slice_t = k[layer_id][idx][:kv_s][:].view(kv_s, kv_head, embed).permute(1, 2, 0) # get K^T
+                v_slice = v[layer_id][idx][:kv_s][:].view(kv_s, kv_head, embed).permute(1, 0, 2)
+
+                score = self.group_matmul(heads, kv_head, q_slice, k_slice_t)
+
+                if s is None:
+                    s = score.view([-1, ])
+                else:
+                    s = torch.concat((s, score.view([-1, ])), 0)
+
+                score = 1
+                if not is_multi_layer:
+                    # 当前scale和tor保持一致，模型侧可能传入scale = np.float32(layer_id + 1)
+                    scale = layer_id + 1
+                score = score * scale
+                
+                if is_clamp == 1:
+                    clamp_min_brc = torch.ones(score.shape) * clamp_min
+                    clamp_max_brc = torch.ones(score.shape) * clamp_max
+                    score = torch.max(score, clamp_min_brc)
+                    score = torch.min(score, clamp_max_brc)
+                if is_mask:
+                    score = score + mask[idx, :q_s, :kv_s]
+
+                score_max = torch.max(score, axis=-1)
+                score = score - score_max.reshape((heads, q_s, 1))
+                score_exp = torch.exp(score)
+                score_sum = torch.sum(score_exp, axis=-1)
+
+                if _p is None:
+                    _p = score_exp.reshape([-1, ])
+                else:
+                    _p = torch.concat((_p, score_exp.reshape([-1, ])), 0)
+
+                p = (score_exp / score_sum.reshape((heads, q_s, 1)))
+                o = self.group_matmul(heads, kv_head, p, v_slice)
+                o = o.view(heads, q_s, embed).permute(1, 0, 2).contiguous()
+                if out is None:
+                    out = o
+                else:
+                    out = torch.concat((out, o), 0)
+                
+                q_offset += q_s
+                k_offset += max_seq
+                v_offset += max_seq
+            
+            # golden data
+            out = out.view(q_ntokens, heads * embed)
+            golden_out = out.to(q.dtype)
+            return [golden_out]
+
+        calc_type = self.op_param.get("calcType", CalcType.UNDEFINED)
+        logger_text = f"CalcType: {calc_type}"
+        logger.debug(logger_text)
+
+        if calc_type == CalcType.PA_ENCODER:
+            q = in_tensors[0]
+            k = in_tensors[1]
+            v = in_tensors[2]
+            q = OpcheckUnpadSelfAttentionOperation.reshape_qkv(q)
+            k = OpcheckUnpadSelfAttentionOperation.reshape_qkv(k)
+            v = OpcheckUnpadSelfAttentionOperation.reshape_qkv(v)
+            mask = None
+            seq_len = None
+            mask_type = self.op_param.get("maskType", MaskType.MASK_TYPE_UNDEFINED)
+            if mask_type != MaskType.MASK_TYPE_UNDEFINED:
+                mask = in_tensors[3]
+                seq_len = in_tensors[4]
+            else:
+                seq_len = in_tensors[3]
+            if mask is not None:
+                if self.get_soc_version() != "Ascend910B":
+                    mask = mask.contiguous().permute(0, 2, 1, 3)
+                    dim0, dim1, dim2, dim3 = mask.shape
+                    mask = mask.contiguous().view(dim0, dim1, dim2 * dim3)
+                    mask = mask[:, :, : dim1]
+                    batch = len(seq_len)
+                    if dim0 == 1:
+                        mask = mask.contiguous().view(dim1, dim1)
+                    elif dim1 != batch:
+                        if dim1 > dim2 * dim3:
+                            # alibi mask compress
+                            step = dim2 * dim3
+                            mask_padded = -10000 * torch.ones(dim0, dim1 * 2, dim1)
+                            for i in range(dim1 // step):
+                                dim1_offset = i * step
+                                dim2_offset = i * step
+                                mask_padded[:, dim1_offset : dim1_offset + dim1, dim2_offset : dim2_offset + step] = mask
+                            mask = mask_padded[:, :dim1, :dim1]
+            in_tensors = [q, k, v, mask, seq_len]
+            asdops_params = OpcheckUnpadSelfAttentionOperation.get_asdops_param(in_tensors, self.op_param)
+        elif calc_type == CalcType.ENCODER:
+            pass
         else:
-            logger_text = f"CalcType: {calc_type}. Using encoder of PageAttention!"
-            logger.debug(logger_text)
-            out = self.pa_encoder_golden_func(in_tensors)
+            pass
+            
         return [out]
 
     def test(self):
