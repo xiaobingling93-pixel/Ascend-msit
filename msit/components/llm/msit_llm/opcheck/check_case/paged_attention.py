e5b2a095f0361ce974d25c52dcf09d9c81775a11
xuchuan18 <xuchuan182024@163.com>
2024-08-03 18:34:07 +0800

    修改PA

--- msit/components/llm/msit_llm/opcheck/check_case/paged_attention.py
+++ msit/components/llm/msit_llm/opcheck/check_case/paged_attention.py
@@ -27,7 +27,7 @@ class CompressType(Enum):
 
 class MaskType(Enum):
     UNDEFINED = 0 # 默认值，全0的mask
-    MASK_TYPE_NONE = 1 # 倒三角mask
+    MASK_TYPE_NORM = 1 # 倒三角mask
     MASK_TYPE_ALIBI = 2 # alibi mask
     MASK_TYPE_SPEC = 3 # 并行解码mask
 
@@ -38,66 +38,34 @@ class QuantType(Enum):
 
 
 class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
-    @staticmethod
-    def get_asdops_param(in_tensors, op_param):
-        asdops_param = {}
-        asdops_param['head_num'] = op_param.get('headNum', 0)
-        asdops_param['qkScale'] = op_param.get('qkScale', 1.0)
-        asdops_param['num_tokens'], _, asdops_param['head_size'] = in_tensors[0].shape # q shape
-        asdops_param['kv_heads'] = op_param.get('kvHeadNum', 0)
-        if op_param['kvHeadNum'] == 0:
-            asdops_param['kv_heads'] = asdops_param['head_num']
-        if op_param['compressType'] == CompressType.COMPRESS_TYPE_KVHEAD.value:
-            asdops_param['compressHead'] = True
-        else:
-            asdops_param['compressHead'] = False
-        context_lens = in_tensors[4]
-        asdops_param['max_context_len'] = max(context_lens)
-        if op_param['maskType'] != MaskType.UNDEFINED.value:
-            asdops_param['mask_dim'] = len(in_tensors[5].shape) # mask shape
-            asdops_param['mask_data_type'] = in_tensors[5].dtype
-            if op_param['maskType'] == MaskType.MASK_TYPE_NONE.value:
-                asdops_param['mask_dim'] = 3
-        else:
-            asdops_param['mask_dim'] = 0
-            asdops_param['mask_data_type'] = torch.bfloat16
-
-        if op_param['quantType'] != QuantType.TYPE_QUANT_UNDEFINED.value:
-            asdops_param['is_int8_flag'] = True
-            if op_param['hasQuantOffset'] == True:
-                asdops_param['has_bias'] = True
-                asdops_param['offset1'] = in_tensors[8]
-                asdops_param['de_scale1_fp32'] = in_tensors[7]
-                asdops_param['offset2'] = in_tensors[10]
-                asdops_param['de_scale2_fp32'] = in_tensors[9]
+    def group_matmul(self, head_num, kv_head_num, A, B, is_k):
+        quant_type = self.op_param.get('quantType', QuantType.TYPE_QUANT_UNDEFINED.value)
+        has_quant_offset = self.op_param.get('hasQuantOffset', False)
+        if quant_type!= QuantType.TYPE_QUANT_UNDEFINED.value:
+            is_int8_flag = True
+            if has_quant_offset:
+                has_bias = True
+                offset1 = self.in_tensors[8]
+                de_scale1_fp32 = self.in_tensors[7]
+                offset2 = self.in_tensors[10]
+                de_scale2_fp32 = self.in_tensors[9]
             else:
-                asdops_param['has_bias'] = False
-                asdops_param['de_scale1_fp32'] = in_tensors[7]
-                asdops_param['de_scale2_fp32'] = in_tensors[8]
-                asdops_param['offset1'] = None
-                asdops_param['offset2'] = None
+                has_bias = False
+                de_scale1_fp32 = self.in_tensors[7]
+                de_scale2_fp32 = self.in_tensors[8]
+                offset1 = None
+                offset2 = None
         else:
-            asdops_param['is_int8_flag'] = False
-            asdops_param['has_bias'] = False
-            asdops_param['de_scale1_fp32'] = None
-            asdops_param['de_scale2_fp32'] = None
-            asdops_param['offset1'] = None
-            asdops_param['offset2'] = None
-        
-        return asdops_param
+            is_int8_flag = False
+            has_bias = False
+            de_scale1_fp32 = None
+            de_scale2_fp32 = None
+            offset1 = None
+            offset2 = None
 
-    @staticmethod
-    def group_matmul(asdops_param, head_num, group_num, A, B, is_k):
-        is_int8_flag = asdops_param['is_int8_flag']
-        has_bias = asdops_param['has_bias']
-        offset1 = asdops_param['offset1']
-        de_scale1_fp32 = asdops_param['de_scale1_fp32']
-        offset2 = asdops_param['offset2']
-        de_scale2_fp32 = asdops_param['de_scale2_fp32']
-
-        group_head = head_num // group_num
+        group_head = head_num // kv_head_num
         score = None
-        for i in range(group_head):
+        for i in range(kv_head_num):
             if is_int8_flag:
                 int8_B = B[i:, (i+1), :, :, ]
                 head_dim = int8_B.shape[2]
@@ -123,24 +91,21 @@ class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
         logger.debug(logger_text)
         return score
 
-    @staticmethod
-    def ref_masked_attention(asdops_param, masked_attention_input, alibi_bias=None):
+    def ref_masked_attention(self, masked_attention_input, alibi_bias=None):
         query = masked_attention_input[0] # (1, head_num, head_size)
-        key = masked_attention_input[1] # (context_len, kv_heads, head_size)
+        key = masked_attention_input[1] # (context_len, kv_head_num, head_size)
         value = masked_attention_input[2]
         scale = masked_attention_input[3] # float
-
-        is_int8_flag = asdops_param['is_int8_flag']
-        mask_data_type = asdops_param['mask_data_type']
 
         # Q * K.T
         query = query * scale
         query = torch.permute(query, (1, 0, 2))
-        if not is_int8_flag:
+        quant_type = self.op_param.get('quantType', QuantType.TYPE_QUANT_UNDEFINED.value)
+        if quant_type == QuantType.TYPE_QUANT_UNDEFINED.value:
             key = torch.permute(key, (1, 2, 0)) # 0 1 2
         else:
             key = torch.permute(key, (1, 0, 2))
-        sim = OpcheckPagedAttentionAttentionOperation.group_matmul(asdops_param, query.shape[0], key.shape[0], query, key, 1) # (head_num, q_seqlen, k_seqlen)
+        sim = self.group_matmul(query.shape[0], key.shape[0], query, key, 1) # (head_num, q_seqlen, k_seqlen)
         if alibi_bias is None:
             sim = sim * scale
         else:
@@ -154,41 +119,43 @@ class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
         p = sim / row_sum
         # P * V
         value = torch.permute(value, (1, 0, 2))
-        out = OpcheckPagedAttentionAttentionOperation.group_matmul(asdops_param, query.shape[0], key.shape[0], p, value, 0)
+        out = self.group_matmul(query.shape[0], key.shape[0], p, value, 0)
         out = torch.permute(out, (1, 0, 2))
         return out
 
-    @staticmethod
-    def ref_single_query_cached_kv_attention(asdops_param, output, paged_input, mask) -> None:
+    def ref_single_query_cached_kv_attention(self, output, paged_input, mask) -> None:
         query = paged_input[0]
-        key_cache = paged_input[1] # (num_blocks, block_size, kv_heads, head_size)
-        value_cache = paged_input[2] # (num_blocks, block_size, kv_heads, head_size)
+        key_cache = paged_input[1] # (num_blocks, block_size, kv_head_num, head_size)
+        value_cache = paged_input[2] # (num_blocks, block_size, kv_head_num, head_size)
         block_tables = paged_input[3]
         context_lens = paged_input[4]
 
-        num_tokens = asdops_param["num_tokens"]
-        kv_heads = asdops_param["kv_heads"]
-        head_num = asdops_param["head_num"]
-        head_size = asdops_param["head_size"]
-        compress_head = asdops_param["compressHead"]
-        max_context_len = asdops_param["max_context_len"]
-        mask_dim = asdops_param["mask_dim"]
-        mask_data_dim = asdops_param["mask_data_type"]
+        head_num = self.op_param.get('headNum', 0)
+        kv_head_num = self.op_param.get('kvHeadNum', 0)
+        num_tokens, _, head_size = query.shape
+        _, block_size, _, _ = key_cache.shape
+        max_context_len = max(context_lens)
+
+        mask_type = self.op_param.get('maskType', MaskType.UNDEFINED.value)
+        if mask_type != MaskType.UNDEFINED.value:
+            mask_dim = len(mask) # mask shape
+            if mask_type == MaskType.MASK_TYPE_NORM.value:
+                mask_dim = 3
+        else:
+            mask_dim = 0
         mask_index_coff = 1
-        if compress_head:
-            query = query.view(num_tokens * kv_heads, head_num // kv_heads, head_size)
-            output = output.view(num_tokens * kv_heads, head_num // kv_heads, head_size)
+
+        compress_type = self.op_param.get('compressType', CompressType.COMPRESS_TYPE_UNDEFINED.value)
+        if compress_type == CompressType.COMPRESS_TYPE_KVHEAD.value:
+            query = query.view(num_tokens * kv_head_num, head_num // kv_head_num, head_size)
+            output = output.view(num_tokens * kv_head_num, head_num // kv_head_num, head_size)
             if mask_dim == 4:
                 mask_shape = mask.shape
-                mask = mask.view(mask_shape[0] * kv_heads, head_num // kv_heads, 1, max_context_len)
+                mask = mask.view(mask_shape[0] * kv_head_num, head_num // kv_head_num, 1, max_context_len)
             else:
-                mask_index_coff = kv_heads
-        head_num = query.shape[1]
-        kv_heads = value_cache.shape[2]
-        head_size = value_cache.shape[3]
-        block_size = value_cache.shape[1]
-        num_input_tokens = query.shape[0]
-        for i in range(len(context_lens)):
+                mask_index_coff = kv_head_num
+
+        for i in range(num_tokens):
             block_table = block_tables[i]
             context_len = int(context_lens[i])
             if context_len == 0:
@@ -201,40 +168,34 @@ class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
                 block_number = int(block_table[j // block_size])
                 block_offset = j % block_size
                 k = key_cache[block_number, block_offset, :, :]
-                k = k.reshape(kv_heads, head_size)
+                k = k.reshape(kv_head_num, head_size)
                 keys.append(k)
                 v = value_cache[block_number, block_offset, :, :]
-                v = v.reshape(kv_heads, head_size)
+                v = v.reshape(kv_head_num, head_size)
                 values.append(v)
             keys = torch.stack(keys, axis=0)
             values = torch.stack(values, axis=0)
-            scale = asdops_param['qkScale']
+            scale = self.op_param.get('qkScale', 1.0)
             masked_attention_input = [q, keys, values, scale]
             if mask_dim == 4:
-                out = OpcheckPagedAttentionAttentionOperation.ref_masked_attention(asdops_param, masked_attention_input, mask[i, :, :1, :context_len])
+                out = self.ref_masked_attention(masked_attention_input, mask[i, :, :1, :context_len])
                 out = out.reshape(head_num, head_size)
             elif mask_dim == 3:
-                out = OpcheckPagedAttentionAttentionOperation.ref_masked_attention(asdops_param, masked_attention_input, mask[i // mask_index_coff, :1, :context_len])
+                out = self.ref_masked_attention(masked_attention_input, mask[i // mask_index_coff, :1, :context_len])
                 out = out.reshape(head_num, head_size)
             else:
-                out = OpcheckPagedAttentionAttentionOperation.ref_masked_attention(asdops_param, masked_attention_input, mask)
+                out = self.ref_masked_attention(masked_attention_input, mask)
                 out = out.reshape(head_num, head_size)
 
             output[i] = out
 
-    @staticmethod
-    def cache_nz_2_nd(cache, kv_heads, embedding_size):
-        cache = cache.permute(0, 2, 1, 3)
-        dim0, dim1, dim2, dim3 = cache.shape
-        cache = cache.contiguous().view(dim0, dim1, dim2 * dim3)
-        cache = cache[:, :, :embedding_size * kv_heads]
-        cache = cache.contiguous().view(dim0, dim1, kv_heads, embedding_size)
-        return cache
-
     def golden_calc(self, in_tensors):
-        asdops_param = OpcheckPagedAttentionAttentionOperation.get_asdops_param(in_tensors, self.op_param)
         query, key_cache, value_cache, block_tables, context_lens = in_tensors[:5]
         mask = None
+
+        head_num = self.op_param.get('headNum', 0)
+        kv_head_num = self.op_param.get('kvHeadNum', 0)
+        num_tokens, _, head_size = query.shape
 
         mask_type = self.op_param.get('maskType', MaskType.UNDEFINED.value)
         if mask_type != MaskType.UNDEFINED.value:
@@ -242,11 +203,9 @@ class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
 
         soc_version = self.get_soc_version()
         if soc_version == 'Ascend310P':
-            kv_heads = self.op_param.get('kvHeads', 1)
-            embedding_size = query.shape[-1]
-
-            key_cache = OpcheckPagedAttentionAttentionOperation.cache_nz_2_nd(key_cache, kv_heads, embedding_size)
-            value_cache = OpcheckPagedAttentionAttentionOperation.cache_nz_2_nd(value_cache, kv_heads, embedding_size)
+            num_blocks, _, block_size, _ = key_cache.shape
+            key_cache = key_cache.permute(0, 2, 1, 3).reshape(num_blocks, block_size, kv_head_num, head_size)
+            value_cache = value_cache.permute(0, 2, 1, 3).reshape(num_blocks, block_size, kv_head_num, head_size)
 
             if mask_type != MaskType.UNDEFINED.value:
                 # mask nz to nd
@@ -255,7 +214,6 @@ class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
                 mask = mask.contiguous().view(dim0, dim1, dim2 * dim3)
                 if mask_type == MaskType.MASK_TYPE_ALIBI.value:
                     batch = len(context_lens)
-                    head_num = self.op_param.get('headNum', 0)
                     if dim0 != head_num:
                         mask = mask.contiguous().view(batch, head_num, dim1, dim2 * dim3)
                     else:
@@ -263,7 +221,7 @@ class OpcheckPagedAttentionAttentionOperation(operation_test.OperationTest):
 
         ref_output = torch.zeros_like(query)
         paged_input = [query, key_cache, value_cache, block_tables, context_lens]
-        OpcheckPagedAttentionAttentionOperation.ref_single_query_cached_kv_attention(asdops_param, ref_output, paged_input, mask)
+        self.ref_single_query_cached_kv_attention(ref_output, paged_input, mask)
         return [ref_output]
 
     def test(self):
