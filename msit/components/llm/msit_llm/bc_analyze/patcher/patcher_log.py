238de2a059ade66791ad1a2a0f303fefe5324078
liujiawang <anonymousdev@163.com>
2024-08-06 17:13:43 +0800

    fifth ver, change logs

--- msit/components/llm/msit_llm/bc_analyze/patcher/patcher_log.py
+++ msit/components/llm/msit_llm/bc_analyze/patcher/patcher_log.py
@@ -1,19 +1,26 @@
 import logging
 import os
+from msit_llm.bc_analyze import get_timestamp
 
 
 logger = logging.getLogger("msit_llm_patcher")
 logger.setLevel(logging.DEBUG)
+logger.propagate = False
 
 logger_dir = os.path.expanduser('~/.msit_cache')
-os.makedirs(logger_dir, 0o500, exist_ok=True)
-file_name = os.path.join(logger_dir, 'patcher.log')
+os.makedirs(logger_dir, 0o700, exist_ok=True)
+file_name = os.path.join(logger_dir, 'msit_patcher.log')
 
 file_handler = logging.FileHandler(
     filename=file_name,
-    mode='w',
+    mode='a',
     encoding='utf-8',
 )
-formatter = logging.Formatter('[%(asctime)s]: %(message)s')
+file_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(asctime)s] [%(name)s]: '%(message)s'")
 file_handler.setFormatter(formatter)
+
 logger.addHandler(file_handler)
+
+os.chmod(file_name, 0o600)
