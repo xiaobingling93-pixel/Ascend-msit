d9399f94bfc0262067259a3e5da234c1c63da733
xuchuan18 <xuchuan182024@163.com>
2024-08-07 15:35:00 +0800

    修改老旧代码

--- msit/components/llm/msit_llm/opcheck/check_case/reduce.py
+++ msit/components/llm/msit_llm/opcheck/check_case/reduce.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from enum import Enum
 import torch
 import torch_npu
 
@@ -19,14 +20,26 @@ from msit_llm.opcheck import operation_test
 from msit_llm.common.log import logger
 
 
+class ReduceType(Enum):
+    REDUCE_UNDEFINED = 0
+    REDUCE_MAX = 1
+    REDUCE_MIN = 2
+    REDUCE_SUM = 3
+
+
 class OpcheckReduceOperation(operation_test.OperationTest):
     def golden_calc(self, in_tensors):
-        op_type = self.op_param.get('reduceType', 0)
-        self.validate_enum_range(op_type, [1, 2, 3])
+        op_type = self.op_param.get('reduceType', ReduceType.REDUCE_UNDEFINED.value)
+        reduce_type_support_list = [
+            ReduceType.REDUCE_MAX.value,
+            ReduceType.REDUCE_MIN.value,
+            ReduceType.REDUCE_SUM.value
+        ]
+        self.validate_int_range(op_type, reduce_type_support_list, "reduceType")
         axis = self.op_param.get('axis', None)
-        if op_type == 1:
+        if op_type == ReduceType.REDUCE_MAX.value:
             return [in_tensors[0].amax(axis)[0]]
-        elif op_type == 2:
+        elif op_type == ReduceType.REDUCE_MIN.value:
             return [in_tensors[0].amin(axis)[0]]
         else:
             return [torch.sum(in_tensors[0], axis)]
