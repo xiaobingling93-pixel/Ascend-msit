f56360de98d0b79faed1959586222e60a0dcf074
饶纳新 <raonaxin@163.com>
2024-08-08 19:43:29 +0800

    新增atb参数，若输入该参数，则生成适配该路径的python文件，这些python文件可以用来加载调用该路径下定义的模型。

--- msit/components/llm/msit_llm/transform/torch_to_float_atb/transform_float.py
+++ msit/components/llm/msit_llm/transform/torch_to_float_atb/transform_float.py
@@ -163,8 +163,28 @@ def save_json(dic, name, save_name=None, save_dir=None):
         json.dump(dic, ff, indent=4)
     logger.info(f"model info saved: {json_save_path}")
     
-
+def check_atb_model_path(atb_model_path):
+    # if atb_model_path != '' and not Path(atb_model_path).exists():
+    #     raise ValueError(f"{atb_model_path} not exists, please check.")
+    if atb_model_path == '':
+        return []
+    if Path(atb_model_path).is_dir():
+        atb_files = list(Path(atb_model_path).glob('*.h')) + list(Path(atb_model_path).glob('*.cpp'))
+    else: 
+        atb_files = []
+        for ff in atb_model_path.split(','):
+            fp = Path(ff)
+            if fp.is_file() and fp.suffix in ['.cpp', '.h']:
+                atb_files.append(str(fp))        
+    if len(atb_files) != 2:
+        raise FileNotFoundError(f"Couldn't parse files in {atb_model_path} automatically 
+                            because there should be only one .cpp file and one .h files. 
+                            Found {len(atb_files)} files.")
+    return atb_files
+                
 def transform_float(source_path, save_name=None, save_dir=None, atb_model_path=''):
+    atb_files = check_atb_model_path(atb_model_path)
+
     logger.info("Building model using transformers...")
 
     model = build_model(source_path)
@@ -174,12 +194,11 @@ def transform_float(source_path, save_name=None, save_dir=None, atb_model_path='
     parsed_model = parser.get_weight_names(model)
     result_files = transform_float_cpp(parsed_model, save_name, save_dir)
 
-    if atb_model_path == '':
-        cpp_model_files = result_files[:2]
-    else:
-        cpp_model_files = list(Path(atb_model_path).glob('*.cpp')) + list(Path(atb_model_path).glob('*.h'))
-    parsed_model.get('weight_names', {})['model_name_in_atb_framework'] = parser.get_atb_model_names(cpp_model_files)
-    parsed_model['acl_inputs_name'] = parser.get_input_names(cpp_model_files)
+    if atb_files == []:
+        atb_files = result_files[:2]
+
+    parsed_model.get('weight_names', {})['model_name_in_atb_framework'] = parser.get_atb_model_names(atb_files)
+    parsed_model['acl_inputs_name'] = parser.get_input_names(atb_files)
     result_files += transform_float_py(parsed_model, save_name, save_dir)
 
     fp_name = parsed_model.get("weight_names", {}).get("model_name", "").lower()
