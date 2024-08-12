63aa047113673cd76393b35f9a46a3770ff822ca
libowen <870390541@qq.com>
2024-08-12 15:39:46 +0800

    add msmodelslim codes

---
+++ msmodelslim/pytorch/llm_ptq/anti_outlier/__init__.py
@@ -0,0 +1,7 @@
+# Copyright Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+
+from .anti_outlier import AntiOutlier
+from .config import AntiOutlierConfig
+from .graph_utils import NormBias
+
+__all__ = ['AntiOutlier', 'AntiOutlierConfig', 'NormBias']
