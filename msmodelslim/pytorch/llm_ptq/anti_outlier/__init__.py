e7638d2ebdaa034f3eae47350fa3a8d6ce728bcb
libowen <870390541@qq.com>
2024-08-12 20:46:57 +0800

    remove code

--- msmodelslim/pytorch/llm_ptq/anti_outlier/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
-
-from .anti_outlier import AntiOutlier
-from .config import AntiOutlierConfig
-from .graph_utils import NormBias
-
-__all__ = ['AntiOutlier', 'AntiOutlierConfig', 'NormBias']
--- msmodelslim/pytorch/llm_ptq/llm_ptq_tools/quant_config/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
-
-from .quant_config import QuantConfig
-
-__all__ = [
-    'QuantConfig',
-]
