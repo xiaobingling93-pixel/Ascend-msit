12d9163827c15eb1a056dea336df2dfe702cb194
xuchuan18 <xuchuan182024@163.com>
2024-08-27 15:27:55 +0800

    添加bind_idx

--- msit/components/llm/msit_llm/opcheck/check_case/self_attention.py
+++ msit/components/llm/msit_llm/opcheck/check_case/self_attention.py
@@ -229,6 +229,7 @@ class OpcheckUnpadSelfAttentionOperation(operation_test.OperationTest):
         if is_mask:
             seq_len = in_tensors[4]
             mask = self.get_mask(in_tensors, seq_len)
+            self.bind_idx.append(4)
         else:
             seq_len = in_tensors[3]
             self.bind_idx.append(3)
