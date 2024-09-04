e498333423fed927f132cc6f68667faa8c698508
Chen, Zhentao <chenzhentao8@huawei.com>
2024-09-04 17:27:14 +0800

    fix dump env

--- msit/components/llm/msit_llm/dump/mietorch/__init__.py
+++ msit/components/llm/msit_llm/dump/mietorch/__init__.py
@@ -13,8 +13,12 @@
 # limitations under the License.
 import os
 
-mindie_rt_dump_so_path = "tools/ait_backend/mindie_torch_dump"
+
+mindie_rt_dump_so_path = "tools/ait_backend/mindie_torch_dump/libmindiedump.so"
+if os.environ.get("ASCEND_TOOLKIT_HOME", None) is None:
+    raise EnvironmentError("Please set ASCEND_TOOLKIT_HOME by running set_env.sh.")
 mindie_rt_dump_so_path = os.path.join(os.environ["ASCEND_TOOLKIT_HOME"], mindie_rt_dump_so_path)
+
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 mindie_rt_dump_config = os.path.join(cur_dir, "acl.json")
 os.environ["LD_PRELOAD"] = f'{mindie_rt_dump_so_path}:{os.environ.get("LD_PRELOAD", "")}'
