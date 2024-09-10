4345fd107c73c4a092a373a9fa53955daea60d5a
liujiawang <anonymousdev@163.com>
2024-09-10 18:02:55 +0800

    [Bug Fixed] output directory should not be created only when there is not error found. In addition, the directory permission should be an octal number instead of decimal

--- msit/components/llm/msit_llm/errcheck/process.py
+++ msit/components/llm/msit_llm/errcheck/process.py
@@ -30,16 +30,12 @@ def handles_check_type(args) -> None:
 def handles_output_dir(args) -> None:
     """Set output directory for backend usage from user input."""
     output_dir = args.output
+
     if not output_dir:
-        # set default directory to current work directory
         output_dir = os.getcwd()
-        logger.warning("Output directory is not provided. "
-                       "Results will be stored under the default directory instead.")
     else:
-        output_dir = os.path.abspath(output_dir)
-        if not os.path.exists(output_dir):
-            logger.warning("Specified directory does not exist. directory creating...")
-            os.makedirs(output_dir, mode=750, exist_ok=True)       
+        output_dir = os.path.realpath(output_dir)
+
     os.environ[ATB_OUTPUT_DIR] = output_dir
     
 
@@ -53,35 +49,26 @@ def handles_so_dir() -> None:
     cann_path = os.environ.get(ASCEND_TOOLKIT_HOME, "/usr/local/Ascend/ascend-toolkit/latest")
     
     if not cann_path or not os.path.exists(cann_path):
-        raise OSError("cann_path is invalid, please install cann-toolkit and set the environment variables.")
+        raise OSError("CANN path is invalid, please install ascend toolkit and set the environment variables.")
 
     cur_is_use_cxx11 = is_use_cxx11()
-    logger.info("Info detected from ATB so is_use_cxx11: %s", cur_is_use_cxx11)
-    
     save_tensor_so_name = ATB_PROB_LIB_WITH_ABI if cur_is_use_cxx11 else ATB_PROB_LIB_WITHOUT_ABI
-    
-    # .so lib should and will be built into errcheck directory in the future
+
     save_tensor_so_path = os.path.join(cann_path, "tools", "ait_backend", "dump", save_tensor_so_name)
-    if not os.path.exists(save_tensor_so_path):
-        raise OSError(f"{save_tensor_so_name} is not found in {cann_path}. Try installing the latest cann-toolkit")
+    so_real_path = os.path.realpath(save_tensor_so_path)
+
+    if not os.path.exists(so_real_path):
+        raise OSError(f"{save_tensor_so_name} is not found. Try to install the latest ascend toolkit")
     
-    if not FileStat(save_tensor_so_path).is_basically_legal('read', strict_permission=True):
+    if not FileStat(so_real_path).is_basically_legal('read', strict_permission=True):
         raise OSError(f"{save_tensor_so_name} is illegal, group or others writable file stat is not permitted")
 
-    logger.info("Append save_tensor_so_path: %s to LD_PRELOAD", save_tensor_so_path)
-    ld_preload = os.getenv(LD_PRELOAD)
-    ld_preload = ld_preload or ""
-    os.environ[LD_PRELOAD] = save_tensor_so_path + ":" + ld_preload
+    os.environ[LD_PRELOAD] = so_real_path + ":" + os.environ.get(LD_PRELOAD, "")
 
 
 def handles_exec(args) -> None:
     """handles executable subcommand from user input."""
     
-    # According to python official document about 'subprocess.run',
-    # if the first argument 'args' has only one string,
-    # the parameter `shell`, should set to False
-    # but all of us set to False by default
-    # hence need to take care of the subcommand that is only consist of spaces
     if not args.exec or args.exec.isspace():
         raise ValueError("exec expected executable subcommand, got empty instead")
     
@@ -94,7 +81,7 @@ def handles_exec(args) -> None:
 
 def process_error_check(args) -> None:    
     logger.info("Environment configuring...")
-    
+
     logger.info("User inputs verifying...")
     handles_check_type(args)
     handles_output_dir(args)
@@ -108,4 +95,4 @@ def process_error_check(args) -> None:
     logger.info("Environment configuration finished. Inference processing...") 
     handles_exec(args)
     logger.info("Inference finished.")
-    logger.info("Results are stored under the directory: %s.", os.environ['ATB_OUTPUT_DIR'])
+    
