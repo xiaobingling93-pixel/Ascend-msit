a1bd9428e47336549a80a776a149405a08f58787
yuliangbin <y1416490440@163.com>
2024-09-10 15:37:01 +0800

    [移除非商发特性]移除transplt组件

--- msit/install.bat
+++ msit/install.bat
@@ -30,11 +30,7 @@ SET all_uninstall=
 
 :loop
 IF NOT "%1"=="" (
-    IF "%1"=="--transplt" (
-        SET select_transplt=1
-        SET all_component=0
-        SHIFT
-    ) ELSE IF "%1"=="--surgeon" (
+    IF "%1"=="--surgeon" (
         SET select_surgeon=1
         SET all_component=0
         SHIFT
@@ -106,19 +102,14 @@ IF NOT %errorlevel%==0 (
 
 :uninstall_func
 
-IF DEFINED select_transplt (
-    pip3 uninstall msit-transplt %all_uninstall%
-    pip3 uninstall ait-transplt %all_uninstall%
-)
-
 IF DEFINED select_surgeon (
     pip3 uninstall msit-surgeon %all_uninstall%
     pip3 uninstall ait-surgeon %all_uninstall%
 ) 
 
 IF %all_component%==1 (
-    pip3 uninstall msit msit-transplt msit-surgeon %all_uninstall%
-    pip3 uninstall ait ait-transplt ait-surgeon %all_uninstall%
+    pip3 uninstall msit msit-surgeon %all_uninstall%
+    pip3 uninstall ait ait-surgeon %all_uninstall%
 )
 
 GOTO:eof
@@ -142,20 +133,6 @@ IF DEFINED select_surgeon (
     pip3 install "%CURRENT_DIR%/components/debug/surgeon" %arg_force_reinstall%
     IF NOT %errorlevel%==0 (
         ECHO pip install surgeon failed, please check the failure reason.
-        EXIT /B 1
-    )
-)
-
-IF DEFINED select_transplt (
-    @REM install transplt component
-    pip3 install "%CURRENT_DIR%/components/transplt" %arg_force_reinstall%
-    IF NOT %errorlevel%==0 (
-        ECHO pip install transplt failed, please check the failure reason.
-        EXIT /B 1
-    )
-
-    CALL "%CURRENT_DIR%/components/transplt/install.bat" %skip_check_cert% %full_install% %llvm_path% %mingw_w64_path%
-    IF NOT %errorlevel%==0 (
         EXIT /B 1
     )
 )
