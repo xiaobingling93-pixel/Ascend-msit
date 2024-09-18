fc7380d6f58863eeed538164ab6d4ef8f0d1b5fa
何文波 <1010589661@qq.com>
2024-09-18 17:54:48 +0800

    【msmodelslim】precision_tool精度评测工具增加安全校验

--- msmodelslim/msmodelslim/tools/precision_tool/precision_tool.py
+++ msmodelslim/msmodelslim/tools/precision_tool/precision_tool.py
@@ -13,7 +13,7 @@ import pandas as pd
 from transformers import PreTrainedModel, AutoTokenizer
 from tqdm import tqdm
 
-from ascend_utils.common.security import json_safe_load, json_safe_dump, get_valid_write_path, get_valid_read_path
+from ascend_utils.common.security import json_safe_load, json_safe_dump, get_valid_path, get_valid_write_path
 from msmodelslim import logger
 
 supported_dataset = ["boolq", "ceval_0_shot", "ceval_5_shot", "humaneval"]
@@ -68,7 +68,7 @@ class PrecisionTest:
         self.dataset_path = os.path.join(self.script_path, "dataset", self.dataset)
         if not os.path.exists(self.dataset_path):
             raise EnvironmentError(f"Dataset was not found, valid path should be '{self.dataset_path}")
-        self.dataset_path = get_valid_read_path(self.dataset_path)
+        self.dataset_path = get_valid_path(self.dataset_path)
         self.result_file = ""
         self.logger.info("Precision Test is inited.")
 
--- msmodelslim/precision_tool/precision_tool.py
+++ msmodelslim/precision_tool/precision_tool.py
@@ -16,7 +16,7 @@ import numpy as np
 from transformers import PreTrainedModel, AutoTokenizer
 from tqdm import tqdm
 
-from security import json_safe_load, json_safe_dump, get_valid_path, get_valid_write_path, get_valid_read_path
+from security import json_safe_load, json_safe_dump, get_valid_path, get_valid_write_path
 from precision_tool import logger
 from precision_tool import truthfulqa_eval
 
@@ -78,7 +78,7 @@ class PrecisionTest:
         self.dataset_path = os.path.join(self.script_path, "dataset", self.dataset)
         if not os.path.exists(self.dataset_path):
             raise EnvironmentError(f"Dataset was not found, valid path should be '{self.dataset_path}")
-        self.dataset_path = get_valid_read_path(self.dataset_path)
+        self.dataset_path = get_valid_path(self.dataset_path)
         self.result_file = ""
         self.logger.info("Precision test was inited.")
 
