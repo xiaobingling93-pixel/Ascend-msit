47cb13f840e78704f2ba8f614c9231edbb73be90
liujiawang <anonymousdev@163.com>
2024-09-25 13:59:17 +0800

    bc analyze 资料增加，部分修改

--- msit/components/llm/msit_llm/bc_analyze/__init__.py
+++
@@ -1,3 +0,0 @@
-from msit_llm.bc_analyze.analyzer import Analyzer
-from msit_llm.bc_analyze.synthezier import Synthesizer
-from msit_llm.bc_analyze.utils import RandomNameSequence, get_timestamp
--- msit/components/llm/msit_llm/common/utils.py
+++ msit/components/llm/msit_llm/common/utils.py
@@ -16,9 +16,12 @@
 import os
 import argparse
 import re
+
 from components.utils.file_open_check import FileStat
 from msit_llm.common.constant import MAX_DATA_SIZE
 from components.utils.check.rule import Rule
+from msit_llm.common.log import logger
+
 
 STR_WHITE_LIST_REGEX = re.compile(r"[^_A-Za-z0-9\"'><=\[\])(,}{: /.~-]")
 INVALID_CHARS = ['|', ';', '&', '&&', '||', '>', '>>', '<', '`', '\\', '!', '\n']
@@ -166,3 +169,52 @@ def check_data_can_convert_to_int(value):
     if not result:
         raise argparse.ArgumentTypeError("%s can not convert to int." % value)
     return int(value)
+
+
+def load_file_to_read_common_check(value):
+    # invalid charcters
+    if re.search(STR_WHITE_LIST_REGEX, value):
+        logger.error("Invalid character: %r", value)
+        raise ValueError
+    
+    # expand soft link
+    value = os.path.realpath(value)
+    
+    # file name too long, file not exists, directory readable
+    # no need to catch, argparse will handle that
+    try:
+        file_status = os.stat(value)
+    except OSError as e:
+        logger.error("%s: %r", e.strerror, value)
+        raise
+    
+    # not regular file
+    if not os.st.S_ISREG(file_status.st_mode):
+        logger.error("Not a regular file: %r", value)
+        raise ValueError
+    
+    # file size
+    if file_status.st_size > MAX_DATA_SIZE:
+        logger.error("File too large: %r", value)
+        raise ValueError
+    
+    # other writeable
+    if (os.st.S_IWOTH & file_status.st_mode) == os.st.S_IWOTH:
+        logger.error("Vulnerable csv path: %r should not be other writeable", value)
+        raise PermissionError
+
+    # uid
+    cur_euid = os.geteuid()
+    if file_status.st_uid != cur_euid:
+        # not root
+        if cur_euid != 0:
+            logger.error("Inconsistent owner: %r", value)
+            raise PermissionError
+        
+        # root but reading a other writeable file
+        elif (os.st.S_IWGRP & file_status.st_mode) == os.st.S_IWGRP or \
+             (os.st.S_IWUSR & file_status.st_mode) == os.st.S_IWUSR:
+            logger.waring("Privilege escalation risk detected. Trying to read a file that belongs to"
+                          " a normal user and is writeable to the user itself or the user group")
+
+    return value
