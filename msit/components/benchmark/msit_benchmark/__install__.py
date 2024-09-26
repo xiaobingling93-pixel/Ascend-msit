a97f52031a529e72ececb356473f6f7229dad224
liujiawang <anonymousdev@163.com>
2024-09-26 11:46:02 +0800

    [benchmark cleancode]

--- msit/components/benchmark/msit_benchmark/__install__.py
+++ msit/components/benchmark/msit_benchmark/__install__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023-2024 Huawei Technologies Co., Ltd.
+# Copyright (c) 2024-2024 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import os
 import sys
 import subprocess
@@ -40,11 +41,11 @@ class BenchmarkInstall(AitInstaller):
 
         if find_links is not None:
             os.environ["MSIT_INSTALL_FIND_LINKS"] = os.path.realpath(find_links)
-        subprocess.run(["/bin/bash", os.path.abspath(os.path.join(os.path.dirname(__file__), "install.sh"))], shell=False)
+        subprocess.run(["/bin/bash", os.path.abspath(os.path.join(os.path.dirname(__file__), "install.sh"))])
 
     def download_extra(self, dest):
         if sys.platform == "win32":
             return
 
         os.environ["MSIT_DOWNLOAD_PATH"] = os.path.realpath(dest)
-        subprocess.run(["/bin/bash", os.path.abspath(os.path.join(os.path.dirname(__file__), "install.sh"))], shell=False)
+        subprocess.run(["/bin/bash", os.path.abspath(os.path.join(os.path.dirname(__file__), "install.sh"))])
--- msit/components/benchmark/setup.py
+++ msit/components/benchmark/setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023-2024 Huawei Technologies Co., Ltd.
+# Copyright (c) 2024-2024 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from configparser import ConfigParser
 from setuptools import setup, find_packages  # type: ignore
 
@@ -26,7 +27,10 @@ msit_sub_tasks = [
     }
 ]
 
-msit_sub_task_entry_points = [f"{t.get('name')}:{t.get('help_info')} = {t.get('module')}:{t.get('attr')}" for t in msit_sub_tasks]
+msit_sub_task_entry_points = [
+    f"{t.get('name')}:{t.get('help_info')} = {t.get('module')}:{t.get('attr')}" 
+    for t in msit_sub_tasks
+]
 
 setup(
     name="msit-benchmark",
