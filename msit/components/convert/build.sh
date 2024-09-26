4e67fabda469859066750bef2dd00fd9dcf135ac
liujiawang <anonymousdev@163.com>
2024-09-26 12:39:32 +0800

    [convert cleancode]

--- msit/components/convert/build.sh
+++ msit/components/convert/build.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-# Copyright (c) 2023-2024 Huawei Technologies Co., Ltd.
+# Copyright (c) 2024-2024 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 if [ ! "$(command -v pip3)" ];then
   echo "Error: pip3 is not installed."
   exit 1
