6bc51b7d956df1d3e0a7e231f6ef2211a47635a1
liqiang <2199164404@qq.com>
2024-09-30 10:05:06 +0800

    clean code

--- msmodelslim/msmodelslim/pytorch/llm_ptq/llm_ptq_tools/quant_config/__init__.py
+++ msmodelslim/msmodelslim/pytorch/llm_ptq/llm_ptq_tools/quant_config/__init__.py
@@ -1,7 +1,8 @@
 # Copyright Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
 
-from .quant_config import QuantConfig
-
 __all__ = [
     'QuantConfig',
 ]
+
+
+from .quant_config import QuantConfig
