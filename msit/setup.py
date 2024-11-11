923f08317c4e8b8297846396ec87ce9a7ac92ef8
王国伟 <leondgarse@163.com>
2024-11-11 14:11:07 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- msit/setup.py
+++ msit/setup.py
@@ -11,14 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import os
 
 from configparser import ConfigParser
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 config = ConfigParser()
-config.read('./components/config/config.ini')
+config.read("./components/config/config.ini")
 
 abs_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(abs_path, "requirements.txt")) as f:
@@ -34,36 +35,57 @@ msit_sub_tasks = [
 ]
 
 msit_sub_task_entry_points = [
-    f"{t.get('name')}:{t.get('help_info')} = {t.get('module')}:{t.get('attr')}"
-    for t in msit_sub_tasks
+    f"{t.get('name')}:{t.get('help_info')} = {t.get('module')}:{t.get('attr')}" for t in msit_sub_tasks
 ]
 
+
+class DevelopWithShUmask(develop):
+    def run(self):
+        cur_umask, sh_mode = 0o027, 0o750
+        os.umask(cur_umask)
+        develop.run(self)
+        for filepath in self.get_outputs():
+            if filepath.endswith(".sh"):
+                os.chmod(filepath, sh_mode)  # Has to be manually 750, cannot be stat with umask
+
+
+class InstallWithShUmask(install):
+    def run(self):
+        cur_umask, sh_mode = 0o027, 0o750
+        os.umask(cur_umask)
+        install.run(self)
+        for filepath in self.get_outputs():
+            if filepath.endswith(".sh"):
+                os.chmod(filepath, sh_mode)  # Has to be manually 750, cannot be stat with umask
+
+
 setup(
-    name='msit',
-    version='7.0.0c912',
-    description='msIT, MindStudio Inference Tools',
-    long_description_content_type='text/markdown',
-    url=config.get('URL', 'msit_url'),
+    name="msit",
+    version="7.0.0c912",
+    description="msIT, MindStudio Inference Tools",
+    long_description_content_type="text/markdown",
+    url=config.get("URL", "msit_url"),
     packages=find_packages(),
     package_data={
-        '': [
-            'LICENSE',
-            'README.md',
-            '*.txt',
-            '*.bat',
-            '*.sh',
-            '*.cpp',
-            '*.h',
-            '*.ini',
+        "": [
+            "LICENSE",
+            "README.md",
+            "*.txt",
+            "*.bat",
+            "*.sh",
+            "*.cpp",
+            "*.h",
+            "*.ini",
         ]
     },
-    data_files=[('', ['requirements.txt'])],
-    license='Apache-2.0',
-    keywords='msit',
-    python_requires='>=3.7',
+    data_files=[("", ["requirements.txt"])],
+    license="Apache-2.0",
+    keywords="msit",
+    python_requires=">=3.7",
     install_requires=required,
     entry_points={
-        'console_scripts': ['msit=components.__main__:main'],
-        'msit_sub_task': msit_sub_task_entry_points,
+        "console_scripts": ["msit=components.__main__:main"],
+        "msit_sub_task": msit_sub_task_entry_points,
     },
+    cmdclass={"develop": DevelopWithShUmask, "install": InstallWithShUmask},
 )
