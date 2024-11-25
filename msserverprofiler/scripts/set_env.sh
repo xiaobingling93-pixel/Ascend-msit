5f732df68cd24a2d314d7346c6f0178c9e06312c
刘成举-liuchengju <lcj200202513@163.com>
2024-11-25 15:57:30 +0800

    提高易用性，增加两个环境变量

--- msserverprofiler/scripts/set_env.sh
+++ msserverprofiler/scripts/set_env.sh
@@ -17,3 +17,6 @@ SHELL_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SHELL_DIR/../lib
 export PYTHONPATH=$PYTHONPATH:$SHELL_DIR/../python
+export MS_SERVER_PROFILER_INLUDE_DIR=$SHELL_DIR/../include
+export MS_SERVER_PROFILER_LIB_DIR=$SHELL_DIR/../lib
+
