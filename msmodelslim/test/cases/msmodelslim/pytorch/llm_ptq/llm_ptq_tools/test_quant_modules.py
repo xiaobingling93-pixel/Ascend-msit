b2cbbc89138bbdc69d48b76675739e8fea012e37
姚晗 <yaohan8@huawei.com>
2024-12-04 17:32:38 +0800

    完善 llm_ptq_tools 相关测试用例

--- msmodelslim/test/cases/msmodelslim/pytorch/llm_ptq/llm_ptq_tools/test_quant_modules.py
+++ msmodelslim/test/cases/msmodelslim/pytorch/llm_ptq/llm_ptq_tools/test_quant_modules.py
@@ -34,7 +34,7 @@ class TestQuantizer:
         assert torch.equal(result, expected_result)
 
     def test_input_quantizer_tensor_forward_should_return_expected_result(self):
-        # 普通a_bit=8场景的激活的Quantizer.tensor_tensor测试
+        # 普通的a_bit=8场景的激活的Quantizer.tensor_forward测试
         cfg = QuantConfig().weight_activation_quant()
         test_quantizer = Quantizer(
             bit=8, is_signed=True, is_enable=True, is_input=True, cfg=cfg, logger=logger, is_dynamic=False
@@ -42,7 +42,7 @@ class TestQuantizer:
         test_quantizer.init_act_and_observer(cfg)
         in_param_data = torch.tensor([1.0, 1.0, 1.0, 1.0])
 
-        result = test_quantizer.tensor_tensor(in_param_data)
+        result = test_quantizer.tensor_forward(in_param_data)
         input_scale = test_quantizer.input_scale
         input_offset = test_quantizer.input_offset
 
@@ -56,19 +56,19 @@ class TestQuantizer:
         assert torch.equal(input_offset, expected_input_offset)
 
     def test_input_quantizer_tensor_forward_with_dynamic_quant_should_return_expected_result(self):
-        # 普通a_bit=8动态量化场景的激活的Quantizer.tensor_tensor测试
+        # 普通的a_bit=8动态量化场景的激活Quantizer.tensor_forward测试
         cfg = QuantConfig().weight_activation_quant()
         test_quantizer = Quantizer(
             bit=8, is_signed=True, is_enable=True, is_input=True, cfg=cfg, logger=logger, is_dynamic=True
         )
         test_quantizer.init_act_and_observer(cfg)
-        in_param_data = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        in_param_data = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
 
-        result = test_quantizer.tensor_tensor(in_param_data)
+        result = test_quantizer.tensor_forward(in_param_data)
         input_scale = test_quantizer.input_scale
         input_offset = test_quantizer.input_offset
 
-        expected_result = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        expected_result = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
         expected_input_scale, expected_input_offset = linear_quantization_params(
             bit=8, x_min=torch.tensor([1]), x_max=torch.tensor([1]), q_signed=True, sym=False
         )
@@ -85,9 +85,9 @@ class TestQuantizer:
         )
         test_quantizer.init_act_and_observer(cfg)
         test_quantizer.enable_int_infer()
-        in_param_data = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        in_param_data = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
 
-        _ = test_quantizer.tensor_tensor(in_param_data)
+        _ = test_quantizer.tensor_forward(in_param_data)
         test_quantizer.disable_calib()
         result = test_quantizer.tensor_forward(in_param_data)
         input_scale = test_quantizer.input_scale
@@ -95,7 +95,7 @@ class TestQuantizer:
 
         test_quantizer.disable_calib()
 
-        expected_result = torch.tensor([127.0, 127.0, 127.0, 127.0])
+        expected_result = torch.tensor([[127.0, 127.0, 127.0, 127.0]])
         expected_input_scale, expected_input_offset = linear_quantization_params(
             bit=8, x_min=torch.tensor([1]), x_max=torch.tensor([1]), q_signed=True, sym=False
         )
@@ -105,18 +105,18 @@ class TestQuantizer:
         assert torch.equal(input_offset, expected_input_offset)
 
     def test_weight_quantizer_tensor_forward_should_return_expected_result(self):
-        # 普通w_bit=8场景的权重的Quantizer.tensor_tensor测试
-        cfg = QuantConfig().weight_activation_quant()
+        # 普通w_bit=8场景的权重的Quantizer.tensor_forward测试
+        cfg = QuantConfig().weight_quant()
         test_quantizer = Quantizer(
             bit=8, is_signed=True, is_enable=True, is_input=False, cfg=cfg, logger=logger, is_dynamic=False
         )
-        in_param_data = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        in_param_data = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
 
-        result = test_quantizer.tensor_tensor(in_param_data)
+        result = test_quantizer.tensor_forward(in_param_data)
         weight_scale = test_quantizer.weight_scale
         weight_offset = test_quantizer.weight_offset
 
-        expected_result = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        expected_result = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
         _, _, expected_weight_scale, expected_weight_offset = init_weight_quant_normal(
             weight=in_param_data,
             bit=8,
@@ -132,21 +132,21 @@ class TestQuantizer:
         assert torch.equal(weight_offset, expected_weight_offset)
 
     def test_weight_quantizer_tensor_forward_with_int_infer_should_return_expected_result(self):
-        # 普通的w_bit=8量化场景的激活Quantizer.tensor_forward int infer测试
+        # 普通的w_bit=8场景的权重的Quantizer.tensor_forward int infer测试
         cfg = QuantConfig().weight_activation_quant()
         test_quantizer = Quantizer(
             bit=8, is_signed=True, is_enable=True, is_input=False, cfg=cfg, logger=logger, is_dynamic=False
         )
         test_quantizer.enable_int_infer()
-        in_param_data = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        in_param_data = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
 
-        _ = test_quantizer.tensor_tensor(in_param_data)
+        _ = test_quantizer.tensor_forward(in_param_data)
         test_quantizer.disable_calib()
         result = test_quantizer.tensor_forward(in_param_data)
         weight_scale = test_quantizer.weight_scale
         weight_offset = test_quantizer.weight_offset
 
-        expected_result = torch.tensor([127.0, 127.0, 127.0, 127.0])
+        expected_result = torch.tensor([[127.0, 127.0, 127.0, 127.0]])
         _, _, expected_weight_scale, expected_weight_offset = init_weight_quant_normal(
             weight=in_param_data,
             bit=8,
@@ -162,12 +162,12 @@ class TestQuantizer:
         assert torch.equal(weight_offset, expected_weight_offset)
 
     def test_init_weight_quant_normal_should_return_expected_result(self):
-        # 普通的w_bit=8场景的权重Quantizer._init_weight_quant_normal测试
+        # 普通的w_bit=8场景的权重的Quantizer._init_weight_quant_normal测试
         cfg = QuantConfig().weight_activation_quant()
         test_quantizer = Quantizer(
-            bit=8, is_signed=True, is_enable=True, is_input=True, cfg=cfg, logger=logger, is_dynamic=False
+            bit=8, is_signed=True, is_enable=True, is_input=False, cfg=cfg, logger=logger, is_dynamic=False
         )
-        in_param_weight = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        in_param_weight = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
         test_quantizer._init_weight_quant_normal(in_param_weight, y=None)
         weight_scale = test_quantizer.weight_scale
         weight_offset = test_quantizer.weight_offset
@@ -187,18 +187,18 @@ class TestQuantizer:
 
     def test_init_weight_quant_normal_with_hessian_should_return_expected_result(self):
         # 普通的w_bit=8场景的权重的Quantizer._init_weight_quant_normal在GPTQ场景下测试
-        cfg = QuantConfig(w_bit=8, a_bit=8).weight_quant(w_method="GPTQ")
+        cfg = QuantConfig(w_bit=8, a_bit=16).weight_quant(w_method="GPTQ")
         test_quantizer = Quantizer(
             bit=8, is_signed=True, is_enable=True, is_input=False, cfg=cfg, logger=logger, is_dynamic=False
         )
-        in_param_weight = torch.tensor([1.0, 1.0, 1.0, 1.0])
-        in_param_input = torch.tensor([1.0, 1.0, 1.0, 1.0])
+        in_param_weight = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
+        in_param_input = torch.tensor([[1.0, 1.0, 1.0, 1.0]])
 
-        test_quantizer._init_weight_quant_normal(in_param_weight, y=None)
+        test_quantizer._init_weight_quant_normal(in_param_input, y=in_param_input)
         weight_scale = test_quantizer.weight_scale
         weight_offset = test_quantizer.weight_offset
 
-        _, _, expected_weight_scale, expected_weight_offset = init_weight_quant_normal(
+        _, _, expected_weight_scale, expected_weight_offset = init_weight_quant_hessian(
             weight=in_param_weight,
             input=in_param_input,
             bit=8,
