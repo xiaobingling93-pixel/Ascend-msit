2e3568974609e169818a89828045687fbf17987c
俞寅恺 <769293914@qq.com>
2024-12-04 17:54:29 +0800

    补充msmodelsilm fuzz 和 mindspore 相关测试用例

--- msmodelslim/test/ST/ModelSlim/test-case/llm_ptq_ZS_9/run.py
+++ msmodelslim/test/ST/ModelSlim/test-case/llm_ptq_ZS_9/run.py
@@ -16,15 +16,6 @@ LOAD_PATH = f"{os.environ['PROJECT_PATH']}/resource/llm_ptq/llama2_7b/"
 
 calib_dataset = []
 
-def get_calib_dataset(tokenizer, calib_data_list):
-    calib_dataset = []
-    for data in calib_data_list:
-        # 对每个校准数据进行编码，并转换为PyTorch张量
-        inputs = tokenizer([data], return_tensors='pt').to('cpu')
-        logging.info("Encoded input: %s", inputs)  # 使用logging记录输入信息
-        calib_dataset.append([inputs.data['input_ids'], inputs.data['attention_mask']])
-    return calib_dataset
-
 # 加载模型配置
 config = AutoConfig.from_pretrained(LOAD_PATH, trust_remote_code=True)
 # 加载分词器
