87a11cf65db74f6a4c0e9948f777ed2c909b8903
liqiang <2199164404@qq.com>
2024-12-06 17:01:22 +0800

    add st

---
+++ msmodelslim/test/ST/llm_ptq_w8a8_per_token/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf $PROJECT_PATH/output/llm_ptq_w8a8_per_token
+python run.py
---
+++ msmodelslim/test/ST/llm_ptq_w8a8_per_token_moe/run.py
@@ -0,0 +1,28 @@
+import os
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from modelslim.pytorch.llm_ptq.llm_ptq_tools import Calibrator, QuantConfig
+
+fp16_path = f"{os.environ['PROJECT_PATH']}/resource/llm_ptq/MOE_tiny/"
+tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=fp16_path, trust_remote_code=True)
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+tokenizer.pad_token_id = tokenizer.eos_token_id
+model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=fp16_path,
+                                             trust_remote_code=True).cpu()
+
+disable_names = []
+disable_names.append('lm_head')
+quant_config = QuantConfig(
+    a_bit=8,
+    w_bit=8,
+    disable_names=disable_names,
+    dev_type='cpu',
+    pr=1.0,
+    w_sym=True,
+    mm_tensor=False,
+    is_dynamic=True
+)
+calibrator = Calibrator(model, quant_config, calib_data=[], disable_level='L0')
+calibrator.run()
+calibrator.save(f"{os.environ['PROJECT_PATH']}/output/llm_ptq_w8a8_per_token_moe", save_type=["numpy", "safe_tensor"])
+
+print('Save quant weight success!')
