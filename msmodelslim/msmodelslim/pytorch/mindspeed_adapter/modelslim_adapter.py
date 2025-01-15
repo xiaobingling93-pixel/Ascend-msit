2c6b849661361a4daf377941d47bfc7678b9d222
libowen <870390541@qq.com>
2025-01-15 16:52:24 +0800

    bugfix error info
    add example for mindspeed-llm
    
    add readme for prefix

--- msmodelslim/msmodelslim/pytorch/mindspeed_adapter/modelslim_adapter.py
+++ msmodelslim/msmodelslim/pytorch/mindspeed_adapter/modelslim_adapter.py
@@ -98,7 +98,7 @@ class ModelAdapter(nn.Module):
             raise ValueError("forward_step function must callable")
         self.model = self.convert_model(model)
         if dev_type not in _SUPPORTED_DEVICES:
-            raise ValueError("Configuration param `dev_id` cannot be correctly parsed! "
+            raise ValueError("Configuration param `dev_type` cannot be correctly parsed! "
                              "Please make sure a valid device id is input")
         self.device = dev_type
         self.forward_step = forward_step
