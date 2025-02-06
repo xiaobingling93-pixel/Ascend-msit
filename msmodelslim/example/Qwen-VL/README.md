5e11fcd95e63ede624d006a5ec9d399dd510d5bf
h00855061 <hamtszyuen1@huawei.com>
2025-02-06 15:43:15 +0800

    增加qwenvl模型量化资料说明

---
+++ msmodelslim/example/Qwen-VL/README.md
@@ -0,0 +1,50 @@
+# Qwen-VL 量化案例
+
+## 模型介绍
+
+- [Qwen-VL](https://github.com/QwenLM/Qwen-VL) 是阿里云研发的大规模视觉语言模型（Large Vision Language Model, LVLM）。 Qwen-VL 可以以图像、文本、检测框作为输入，并以文本和检测框作为输出。 Qwen-VL 系列模型性能强大，具备多语言对话、多图交错对话等能力，并支持中文开放域定位和细粒度图像识别与理解。
+#### Qwen-VL模型当前已验证的量化方法
+
+- W8A8量化：Qwen-VL
+
+#### 此模型仓已适配的模型版本权重获取地址
+##### Qwen-VL
+- [Qwen-VL](https://huggingface.co/Qwen/Qwen-VL/tree/main)
+
+## 环境配置
+
+- 环境配置请参考[使用说明](https://gitee.com/ascend/msit/blob/master/msmodelslim/README.md)
+
+## 量化权重生成
+
+- 量化权重统一使用[quant_qwenvl.py](./quant_qwenvl.py)脚本生成，以下提供Qwen模型量化权重生成快速启动命令。
+
+
+#### 量化参数说明
+| 参数名 | 含义 | 默认值 | 使用方法 | 
+| ------ | ---- | --- | -------- | 
+| model_path | 浮点权重路径 | 无默认值 | 必选参数；<br>输入Llava权重目录路径。 |
+| calib_images | 校准集图片路径 | ./calibImages | 可选参数；<br>输入校准数据集的目录路径。本示例中图片来源于公开数据集[COCO](https://cocodataset.org/)。 |
+| save_directory | 量化权重路径 | 无默认值 | 必选参数；<br>输出量化结果目录路径。 |
+| a_bit | 激活值量化bit | 8 |大模型量化场景下，可配置为8或16； <br>大模型稀疏量化场景下，需配置为8。 |
+| w_bit | 权重量化bit | 8 | 大模型量化场景下，可配置为8或16； <br>大模型稀疏量化场景下，需配置为4。 |
+| device_type | device类型 | cpu | 可选值：['cpu', 'npu'] |
+| part_file_size | 量化权重文件大小 | 无限制 | 单个量化权重文件大小不超过xGB。|
+
+- 更多参数配置要求，请参考量化过程中配置的参数 [QuantConfig](https://gitee.com/ascend/msit/blob/dev/msmodelslim/docs/Python-API接口说明/大模型压缩接口/大模型量化接口/PyTorch/QuantConfig.md)
+  以及量化参数配置类 [Calibrator](https://gitee.com/ascend/msit/blob/dev/msmodelslim/docs/Python-API接口说明/大模型压缩接口/大模型量化接口/PyTorch/Calibrator.md)
+
+### 使用案例
+- 请将{浮点权重路径}和{量化权重路径}替换为用户实际路径。
+- 如果需要使用npu多卡量化，请先配置环境变量，仅支持1~3卡量化，暂不支持Atlas 300I Duo推理卡：
+  ```shell
+  export ASCEND_RT_VISIBLE_DEVICES=0,1,2
+  export PYTORCH_NPU_ALLOC_CONF=expandable_segments:False
+  ```
+  
+#### 1. Qwen-VL系列
+##### Qwen-VL W8A8量化
+生成Qwen-VL模型量化权重，antioutlier使用m2算法配置，在NPU上进行运算
+  ```shell
+  python quant_qwenvl.py  --model_path {浮点权重路径} --calib_images {校准图片路径}  --save_directory {量化权重保存路径} --w_bit 8 --a_bit 8 --device_type npu
+  ```
---
+++ msmodelslim/example/Qwen-VL/quant_qwenvl.py
@@ -0,0 +1,79 @@
+# Copyright Huawei Technologies Co., Ltd. 2025. All rights reserved.
+import argparse
+import os
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+import torch
+
+from modelslim.pytorch.llm_ptq.anti_outlier import AntiOutlierConfig, AntiOutlier
+from msmodelslim.pytorch.llm_ptq.llm_ptq_tools import Calibrator, QuantConfig
+
+
+CPU = "cpu"
+NPU = "npu"
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, default='')
+    parser.add_argument('--calib_images', type=str, default='./calibImages')
+    parser.add_argument('--save_directory', type=str, default='')
+    parser.add_argument('--part_file_size', type=int, default=None)
+    parser.add_argument('--w_bit', type=int, default=8)
+    parser.add_argument('--a_bit', type=int, default=8)
+    parser.add_argument('--device_type', type=str, choices=[CPU, NPU], default=CPU)
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
+    tokenizer.padding_side = 'left'
+    tokenizer.pad_token_id = tokenizer.eod_id
+
+    ##1.加载模型
+    device_map = CPU if args.device_type == CPU else "auto"
+    config = AutoConfig.from_pretrained(args.model_path, trust_remote_code=True)
+    dtype = config.torch_dtype if args.device_type == NPU else torch.float32
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_path, device_map=device_map, trust_remote_code=True, torch_dtype=torch.float16).eval()
+
+
+    ##2.设置回退层
+    disable_names = [f"transformer.h.{layer}.mlp.c_proj" for layer in range(32)]
+    disable_names.append("transformer.visual.attn_pool.attn.out_proj")
+    disable_names.append("lm_head")
+
+    ##3.校准集
+    images_list = os.listdir(args.calib_images)
+    prompt = "USER: <image>\nDescribe this image in detail. ASSISTANT:"
+    calib_data = []
+    for i in images_list:
+        imagePath = os.path.join(args.calib_images, i)
+        query = tokenizer.from_list_format([
+            {'image': imagePath}, # Either a local path or an url
+            {'text': prompt},
+        ])
+        inputs = tokenizer(query, return_tensors='pt')
+        inputs = inputs.to(model.device)
+        calib_data.append([inputs['input_ids'], None, inputs['attention_mask']])
+
+    ##4.异常值抑制
+    anti_config = AntiOutlierConfig(
+        w_bit=args.w_bit,
+        a_bit=args.a_bit,
+        anti_method="m2",
+        dev_type=args.device_type,
+        dev_id=model.device.index,
+        )
+    anti_outlier = AntiOutlier(model, calib_data=calib_data, cfg=anti_config)
+    anti_outlier.process()
+    ##5.模型量化
+    quant_config = QuantConfig(
+        w_bit=args.w_bit,
+        a_bit=args.a_bit,
+        disable_names=disable_names,
+        dev_type=args.device_type,
+        dev_id=model.device.index,
+        act_method=2,
+        mm_tensor=False,
+    )
+    calibrator = Calibrator(model, quant_config, calib_data=calib_data, disable_level='L0')
+    calibrator.run()
+    ##6.保存权重
+    calibrator.save(args.save_directory, save_type=["safe_tensor"], part_file_size=args.part_file_size)
