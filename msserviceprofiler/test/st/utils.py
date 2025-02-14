c0d7aa6e9ac37bd39082bc67dde8f17af3439b21
che <chepishuai1@h-partners.com>
2025-02-14 10:42:00 +0800

    [AR20250110084791]检视意见修正

--- msserviceprofiler/test/st/utils.py
+++ msserviceprofiler/test/st/utils.py
@@ -1,9 +1,24 @@
+# Copyright (c) 2025-2025 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import subprocess
 import os
 import re
 import logging
 
 COMMAND_SUCCESS = 0
+
 
 def execute_cmd(cmd):
     logging.info('Execute command:%s' % " ".join(cmd))
@@ -16,7 +31,10 @@ def execute_cmd(cmd):
 def check_column_actual(actual_columns, expected_columns, context):
     """检查实际列名是否与预期列名一致"""
     for col in expected_columns:
-        assert col in actual_columns, f"在 {context} 中未找到预期列名: {col}"
+        if col not in actual_columns:
+            print(f"在 {context} 中未找到预期列名: {col}")
+            return False
+    return True
 
 
 def check_row(df, row_index, column):
@@ -26,4 +44,6 @@ def check_row(df, row_index, column):
         # 尝试将值转换为数字
         float(value)
     except (ValueError, KeyError):
-        assert False, f"在 {column} 列的第 {row_index} 行，值 {value} 不是有效的数字"
+        print(f"在 {column} 列的第 {row_index} 行，值 {value} 不是有效的数字")
+        return False
+    return True
