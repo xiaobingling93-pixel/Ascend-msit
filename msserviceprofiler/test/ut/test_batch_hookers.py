c6185ee3248c7292de62d0b44cc71fd695b19f1a
Chen Huiwen <huiwch26@163.com>
2025-03-13 10:43:31 +0800

    ut增加链接调用校验

--- msserviceprofiler/test/ut/test_batch_hookers.py
+++ msserviceprofiler/test/ut/test_batch_hookers.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 import unittest
 import sys
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, call
 from collections import deque
 
 
@@ -36,7 +36,7 @@ class FakeSequenceGroupMetadata:
 # 模拟 BlockManager 行为
 class FakeBlockManager:
     @staticmethod
-    def can_swap_out(self, seq_group):
+    def can_swap_out(self, seq_group=None):
         return True
 
 
@@ -114,6 +114,7 @@ from ms_service_profiler_ext.vllm_profiler.vllm_profiler_core.batch_hookers impo
 )
 
 
+# 测试hook类
 @patch('ms_service_profiler_ext.vllm_profiler.vllm_profiler_core.batch_hookers.Profiler')
 class TestSchedulerHook(unittest.TestCase):
 
@@ -122,7 +123,7 @@ class TestSchedulerHook(unittest.TestCase):
         self.scheduler_hook = SchedulerHook()
         self.llm_engine_hook = LLMEngineHook()
         
-        # 调用 init 方法，定义hook函数
+        # 调用 init 方法，其中的do_hook会将被测试的_maker函数与Fake类中的函数关联起来
         self.scheduler_hook.init()
         self.llm_engine_hook.init()
 
@@ -135,7 +136,8 @@ class TestSchedulerHook(unittest.TestCase):
         self.fake_scheduler = FakeScheduler(self.fake_seq_group)
         self.fake_llm_engine = FakeLLMEngine()
 
-
+    # setUp中的init函数调用之后，调用Fake中原函数，会调用到对应的_maker函数
+    # 校验_maker函数被正确调用，且其中的Profiler打点正确
     def test_schedule_maker(self, mock_profiler):
         seq_group_metadata_list, scheduler_outputs, allow_async_output_proc = \
             self.fake_scheduler.schedule()
@@ -148,45 +150,81 @@ class TestSchedulerHook(unittest.TestCase):
         # 验证Profiler方法正确调用且参数正确
         mock_profiler(Level.INFO).span_start.assert_called_with("BatchSchedule")
 
-    # 调用Fake中原函数，校验调用到对应的_maker函数，且Profiler中打点正确
     def test_abort_seq_group_maker(self, mock_profiler):
         self.fake_scheduler.abort_seq_group(self.fake_request_id)
-        mock_profiler(Level.INFO).res.assert_called_with(self.fake_request_id)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res(self.fake_request_id).metric_inc(
+            'FINISHED_ABORTED', 1).event("ReqState")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_allocate_and_set_running_maker(self, mock_profiler):
         self.fake_scheduler._allocate_and_set_running(self.fake_seq_group)
-        mock_profiler(Level.INFO).res.assert_called_with(self.fake_request_id)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res(self.fake_request_id).metric_inc(
+            'RUNNING', 1).metric_inc('WAITING', -1).event("ReqState")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_preempt_by_recompute_maker(self, mock_profiler):
         self.fake_scheduler._preempt_by_recompute(self.fake_seq_group)
-        mock_profiler(Level.INFO).res.assert_called_with(self.fake_request_id)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res(self.fake_request_id).metric_inc(
+            'RUNNING', -1).metric_inc('WAITING', 1).event("ReqState")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_swap_in_maker(self, mock_profiler):
         self.fake_scheduler._swap_in(self.fake_seq_group)
-        mock_profiler(Level.INFO).res.assert_called_with(self.fake_request_id)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res(self.fake_request_id).metric_inc(
+            'RUNNING', 1).metric_inc('SWAPPED', -1).event("ReqState")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_swap_out_maker(self, mock_profiler):
-        self.fake_scheduler._swap_in(self.fake_seq_group)
-        mock_profiler(Level.INFO).res.assert_called_with(self.fake_request_id)
+        self.fake_scheduler._swap_out(self.fake_seq_group)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res(self.fake_request_id).metric_inc(
+            'RUNNING', -1).metric_inc('SWAPPED', 1).event("ReqState")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_free_finished_seq_groups_maker(self, mock_profiler):
         # 配置 running 队列
         self.fake_scheduler.running.append(self.fake_seq_group)
 
         self.fake_scheduler.free_finished_seq_groups()
-        mock_profiler(Level.INFO).res.assert_called_with(self.fake_request_id)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res(self.fake_request_id).metric_inc(
+            'RUNNING', -1).metric_inc('FINISHED', 1).event("ReqState")
+        mock_profiler.assert_has_calls([expected_call])
 
     # 校验Profiler.res中的request list正确
     def test_add_seq_group_to_running_maker(self, mock_profiler):
         self.fake_scheduler._add_seq_group_to_running(self.fake_seq_group)
-        mock_profiler(Level.INFO).res.assert_called_with([self.fake_request_id])
+        
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res([self.fake_request_id]).metric(
+            "QueueSize", len(self.fake_scheduler.running)).metric_scope('running').event("Enqueue")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_add_seq_group_maker(self, mock_profiler):
         self.fake_scheduler.add_seq_group(self.fake_seq_group)
-        mock_profiler(Level.INFO).res.assert_called_with([self.fake_request_id])
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res([self.fake_request_id]).metric(
+            "QueueSize", len(self.fake_scheduler.waiting)).metric_scope('waiting').event("Enqueue")
+        mock_profiler.assert_has_calls([expected_call])
 
     def test_add_seq_group_to_swapped_maker(self, mock_profiler):
         self.fake_scheduler._add_seq_group_to_swapped(self.fake_seq_group)
+
+        # 校验Profiler方法链式调用正确
+        expected_call = call(Level.INFO).res([self.fake_request_id]).metric(
+            "QueueSize", len(self.fake_scheduler.swapped)).metric_scope('waiting').event("Enqueue")
+        mock_profiler.assert_has_calls([expected_call])
         mock_profiler(Level.INFO).res.assert_called_with([self.fake_request_id])
 
     # 校验队列打点函数queue_profiler正确调用
