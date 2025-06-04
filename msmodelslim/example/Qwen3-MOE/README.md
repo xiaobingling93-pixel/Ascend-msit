74b8f737c0fc111a58128fe09afdfa15bbb362b9
libowen <870390541@qq.com>
2025-06-04 09:56:58 +0800

    add qwen3-moe case

---
+++ msmodelslim/example/Qwen3-MOE/README.md
@@ -0,0 +1,69 @@
+# Qwen3-MOE 量化案例
+
+## 模型介绍
+
+- [Qwen3-235B-A22B](https://huggingface.co/Qwen/Qwen3-235B-A22B)、[Qwen3-30B-A3B](https://huggingface.co/Qwen/Qwen3-30B-A3B) Qwen3 是 Qwen 系列中最新一代大型语言模型，提供全面的密集模型和混合专家 (MoE) 模型。Qwen3 基于丰富的训练经验，在推理、指令遵循、代理能力和多语言支持方面取得了突破性进展。其中Qwen3-MoE结构模型典型代表模型有Qwen3-235B-A22B和Qwen3-30B-A3B。
+
+#### Qwen3-MoE模型当前已验证的量化方法
+
+- W8A8量化：Qwen3-235B-A22B, Qwen3-30B-A3B
+
+#### 此模型仓已适配的模型版本
+
+- [Qwen3-235B-A22B](https://huggingface.co/Qwen/Qwen3-235B-A22B)
+- [Qwen3-30B-A3B](https://huggingface.co/Qwen/Qwen3-30B-A3B)
+
+## 环境配置
+
+- 环境配置请参考[使用说明](https://gitee.com/ascend/msit/blob/master/msmodelslim/README.md)
+- transformers版本需要配置安装4.51.0以上版本
+    - pip install transformers>=4.51.0
+
+## 量化权重生成
+
+- 量化权重可使用[quant_qwen_moe_w8a8.py](./quant_qwen_moe_w8a8.py)脚本生成。
+
+#### quant_qwen_moe_w8a8.py 量化参数说明
+
+| 参数名           | 含义           | 默认值  | 使用方法                          | 
+|---------------|--------------|------|-------------------------------| 
+| model_path    | 浮点权重路径       | 无默认值 | 必选参数；<br>输入DeepSeek权重目录路径。    |
+| save_path     | 量化权重路径       | 无默认值 | 必选参数；<br>输出量化结果目录路径。          |
+| layer_count   | 模型层数       | 0 | 可选参数；<br>用于调试，实际量化的层数。0表示使用所有层。        |
+| anti_dataset  | 反异常值校准数据集路径 | ./anti_prompt_50.json | 可选参数；<br>用于反异常值处理的校准数据集路径。 |
+| calib_dataset | 量化校准数据集路径   | ./calib_prompt_50.json | 可选参数；<br>量化校准集路径。             |
+| batch_size     | 输入batch size | 4  | 可选参数；<br>生成量化校准数据时使用的batch size。batch size越大，校准速度越快，但也要求更多的显存和内存，如资源受限，请降低batch size。  |
+| mindie_format | 是否开启旧的权重配置文件保存格式 | False | 指定`mindie_format=True`保存仅适配MindIE的权重配置文件保存格式。|
+
+
+注：在量化脚本里面通过transformers库对模型进行加载时，调用`from_pretrained`函数时会指定`trust_remote_code=True`让修改后的modeling文件能够正确的被加载。(请确保加载的modeling文件的安全性)
+
+
+更多参数配置要求，请参考量化过程中配置的参数 [QuantConfig](https://gitee.com/ascend/msit/blob/dev/msmodelslim/docs/Python-API接口说明/大模型压缩接口/大模型量化接口/PyTorch/QuantConfig.md)
+以及量化参数配置类 [Calibrator](https://gitee.com/ascend/msit/blob/dev/msmodelslim/docs/Python-API接口说明/大模型压缩接口/大模型量化接口/PyTorch/Calibrator.md)
+
+### 使用案例
+
+- 请将{浮点权重路径}和{量化权重路径}替换为用户实际路径。
+- 如果需要使用npu多卡量化，请先配置环境变量，支持多卡量化：
+  ```shell
+  export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+  export PYTORCH_NPU_ALLOC_CONF=expandable_segments:False
+  ```
+
+#### Qwen3-30B-A3B
+
+##### Qwen3-30B-A3B w8a8 混合量化(Attention:w8a8量化，MoE:w8a8 dynamic量化)
+- 生成Qwen3-30B-A3B模型 w8a8 混合量化权重
+  ```shell
+  python3 quant_qwen_moe_w8a8.py --model_path {浮点权重路径} --save_path {W8A8量化权重路径}
+  ```
+
+#### Qwen3-235B-A22B
+
+##### Qwen3-235B-A22B w8a8 混合量化(Attention:w8a8量化，MoE:w8a8 dynamic量化)
+- 生成Qwen3-235B-A22B模型 w8a8 混合量化权重
+  ```shell
+  python3 quant_qwen_moe_w8a8.py --model_path {浮点权重路径} --save_path {W8A8量化权重路径}
+  ```
+
---
+++ msmodelslim/example/Qwen3-MOE/quant_qwen_moe_w8a8.py
@@ -0,0 +1,159 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+import os
+import sys
+import argparse
+import functools
+import json
+from unittest.mock import patch
+
+import torch
+import torch_npu
+from torch_npu.contrib import transfer_to_npu
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.abspath(os.path.join(current_directory, '..', ".."))
+sys.path.append(parent_directory)
+
+from ascend_utils.common.security import get_valid_read_path, get_write_directory, check_number
+from example.common.utils import SafeGenerator
+from msmodelslim.tools.copy_config_files import copy_config_files, modify_config_json
+from msmodelslim.pytorch.llm_ptq.anti_outlier import AntiOutlierConfig, AntiOutlier
+from msmodelslim.pytorch.llm_ptq.llm_ptq_tools import Calibrator, QuantConfig
+from msmodelslim.tools.logger import set_logger_level
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, help="The path of float model and tokenizer"),
+    parser.add_argument('--save_path', type=str, help="The path to save quant model"),
+    parser.add_argument('--layer_count', type=int, default=0, help="Layer count when loading model")
+    parser.add_argument('--anti_dataset', type=str, default="./anti_prompt_50.json",
+                        help="The calib data for anti outlier")
+    parser.add_argument('--calib_dataset', type=str, default="./calib_prompt_50.json",
+                        help="The calib data for calibration")
+    parser.add_argument('--batch_size', type=int, default=4, help="Batch size for anti and calibration")
+    parser.add_argument('--mindie_format', action="store_true", help="Enable only mindie config save")
+    return parser.parse_args()
+
+
+def custom_hook(model_config):
+    model_config["quantize"] = "w8a8_dynamic"
+
+
+def get_calib_dataset_batch(model_tokenizer, calib_list, batch_size, device="npu"):
+    calib_dataset = []
+    calib_list = [calib_list[i:i + batch_size] for i in range(0, len(calib_list), batch_size)]
+    for calib_data in calib_list:
+        inputs = model_tokenizer(calib_data, return_tensors='pt', padding=True).to(device)
+        calib_dataset.append(
+            [value.to(device) for key, value in inputs.data.items() if isinstance(value, torch.Tensor)])
+    return calib_dataset
+
+
+def main():
+    args = parse_args()
+    set_logger_level("info")
+
+    model_path = args.model_path
+    batch_size = args.batch_size
+
+    save_path = get_write_directory(args.save_path, write_mode=0o750)
+    check_number(batch_size, int, 1, 16, "batch_size")
+
+    safe_generator = SafeGenerator()
+ 
+    config = safe_generator.get_config_from_pretrained(model_path=model_path, 
+                                                       trust_remote_code=True)
+    num_layer = config.num_hidden_layers
+    if args.layer_count < 0 or args.layer_count > num_layer:
+        raise ValueError(
+            f"Invalid value for parameter layer_count: {args.layer_count}."
+            f"Must be between 0 and {num_layer}."
+        )
+    # Set layer count to 0 means use all layers, otherwise it will only use the first layer_count layers
+    config.num_hidden_layers = args.layer_count if args.layer_count != 0 else config.num_hidden_layers
+    # Disable use cache because we don't need to use cache, otherwise it will use too much device memory then cause OOM
+    config.use_cache = False
+
+    tokenizer = safe_generator.get_tokenizer_from_pretrained(model_path=model_path,
+                                                             config=config,
+                                                             trust_remote_code=True,
+                                                             use_fast=True,
+                                                             add_eos_token=True)
+
+    model = safe_generator.get_model_from_pretrained(model_path=model_path,
+                                                     config=config,
+                                                     trust_remote_code=True,
+                                                     device_map={
+                                                         "model.embed_tokens": 0,
+                                                         "model.layers": "cpu",
+                                                         "model.norm": "cpu",
+                                                         "lm_head": 0,
+                                                     },
+                                                     torch_dtype="auto",
+                                                     attn_implementation='eager')
+
+    anti_dataset_path = get_valid_read_path(args.anti_dataset, "json", is_dir=False)
+    calib_dataset_path = get_valid_read_path(args.calib_dataset, "json", is_dir=False)
+    with open(anti_dataset_path, "r") as file:
+        anti_prompt = json.load(file)
+    with open(calib_dataset_path, "r") as file:
+        calib_prompt = json.load(file)
+    anti_dataset = get_calib_dataset_batch(tokenizer, anti_prompt, batch_size, model.device)
+    dataset_calib = get_calib_dataset_batch(tokenizer, calib_prompt, batch_size, model.device)
+    
+    with torch.no_grad():
+        anti_config = AntiOutlierConfig(w_bit=8,
+                                        a_bit=8,
+                                        anti_method='m4',
+                                        dev_type='npu',
+                                        dev_id=model.device.index)
+        anti_outlier = AntiOutlier(model, calib_data=anti_dataset, cfg=anti_config)
+        anti_outlier.process()
+
+    disable_names = []
+    for ids in range(config.num_hidden_layers):
+        disable_names.append("model.layers." + str(ids) + ".mlp.gate")
+
+    quant_config = QuantConfig(
+        a_bit=8,
+        w_bit=8,
+        disable_names=disable_names,
+        dev_type='npu',
+        dev_id=model.device.index,
+        act_method=1,
+        pr=1.0,
+        w_sym=True,
+        mm_tensor=False,
+    )
+
+    calibrator = Calibrator(model, 
+                            quant_config, 
+                            calib_data=dataset_calib, 
+                            disable_level="L0", 
+                            mix_cfg={"*.mlp.*": "w8a8_dynamic", "*": "w8a8"})
+    calibrator.run()
+
+    if args.mindie_format:
+        quant_model_description_json_name = "quant_model_description_w8a8_dynamic.json"
+    else:
+        quant_model_description_json_name = "quant_model_description.json"
+    calibrator.save(save_path,
+                    json_name=quant_model_description_json_name,
+                    safetensors_name="quant_model_weight_w8a8_dynamic.safetensors",
+                    save_type=["safe_tensor"],
+                    part_file_size=4)
+
+    custom_hooks = {
+        'config.json': functools.partial(modify_config_json, custom_hook=custom_hook)
+    }
+    copy_config_files(input_path=model_path, output_path=save_path, quant_config=quant_config,
+                      mindie_format=args.mindie_format, custom_hooks=custom_hooks)
+
+
+if __name__ == "__main__":
+    # torch_npu will fork a new process to init,
+    # it's lazy_init will fail after we load a big model,so we need to init it here
+    torch_npu.npu.init()
+    # Invoke main process
+    main()
