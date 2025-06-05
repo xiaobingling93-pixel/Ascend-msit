3d6e0997f0761e496425ba80b93f1c9f8c3191bb
jhon-117 <fangkai15@huawei.com>
2025-06-05 03:09:38 +0000

    bugfix for modelevalstate
    Signed-off-by: jhon-117 <fangkai15@huawei.com>

--- msserviceprofiler/msserviceprofiler/modelevalstate/run_vllm.sh
+++ msserviceprofiler/msserviceprofiler/modelevalstate/run_vllm.sh
@@ -3,6 +3,6 @@
 pkill -15 'vllm'
 ps -ef | grep 'python'
 npu-smi info
-vllm serve MODEL_PATH --served-model-name MODEL_NAME --host 127.0.0.1 --port 6379
+vllm serve MODEL_PATH --served-model-name MODEL_NAME --host 127.0.0.1 --port 6379 \
 --max-num-batched-tokens $MAX_NUM_BATCHED_TOKENS \
 --max-num-seqs $MAX_NUM_SEQS
