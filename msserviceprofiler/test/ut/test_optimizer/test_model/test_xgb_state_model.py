6abe07a834aa67494fe22cd7398d35f2acc10fff
che <chepishuai1@h-partners.com>
2025-06-12 16:13:14 +0800

    cleancode

--- msserviceprofiler/test/ut/test_optimizer/test_model/test_xgb_state_model.py
+++ msserviceprofiler/test/ut/test_optimizer/test_model/test_xgb_state_model.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest
 from unittest.mock import patch, MagicMock
 from pathlib import Path
 
+import pytest
 import numpy as np
 import xgboost
 
