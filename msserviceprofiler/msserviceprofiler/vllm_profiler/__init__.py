f336ee698c1fdc12a8d9f20ca20cc165a46be820
王国伟 <leondgarse@163.com>
2025-06-28 09:20:53 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- msserviceprofiler/msserviceprofiler/vllm_profiler/__init__.py
+++ msserviceprofiler/msserviceprofiler/vllm_profiler/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 from importlib.metadata import version
 from packaging.version import Version
 
@@ -20,15 +19,12 @@ try:
 except Exception as ee:
     vllm_version = None
 
-def __getattr__(name):
-    print(f">>>> {name = }")
-    if name == 'vllm_profiler':
-        if vllm_version and Version(vllm_version) > Version("0.8.3"):
-            import msserviceprofiler.vllm_profiler.vllm_profiler_0_8_4
-        elif vllm_version and Version(vllm_version) > Version("0.6.2"):
-            import msserviceprofiler.vllm_profiler.vllm_profiler_0_6_3
-        else:
-            import logging
 
-            logging.error(f"Not supported vllm version {vllm_version}")
-    raise AttributeError(f"module 'vllm_profiler' has no attribute '{name}'")
+if vllm_version and Version(vllm_version) > Version("0.8.3"):
+    import msserviceprofiler.vllm_profiler.vllm_profiler_0_8_4
+elif vllm_version and Version(vllm_version) > Version("0.6.2"):
+    import msserviceprofiler.vllm_profiler.vllm_profiler_0_6_3
+else:
+    import logging
+
+    logging.error(f"Not supported vllm version {vllm_version}")
