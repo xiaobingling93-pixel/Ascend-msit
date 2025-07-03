bfbd0800ea3892380b86b987aaa492cdd74456e5
jhon-117 <fangkai15@huawei.com>
2025-07-03 11:53:22 +0000

    clean code
    Signed-off-by: jhon-117 <fangkai15@huawei.com>

--- msserviceprofiler/test/st/optimizer/test_optimizer_light.py
+++ msserviceprofiler/test/st/optimizer/test_optimizer_light.py
@@ -17,11 +17,12 @@ import shutil
 import glob
 from unittest import TestCase
 
+
 def check_csv_no_empty_start(csv_file):
     with open(csv_file, 'r', encoding='utf-8') as f:
         next(f)  # 跳过标题行（第一行）
 
-        for line_num, line in enumerate(f, start=2):  # 从第2行开始检查（数据行）
+        for _, line in enumerate(f, start=2):  # 从第2行开始检查（数据行）
             stripped_line = line.strip()
             
             # 检查是否以逗号开头（即第一个单元格是否为空）
@@ -37,6 +38,7 @@ def get_modelevalstate_path():
     dir_path = os.path.dirname(file_path)
     target_path = os.path.abspath(os.path.join(dir_path, "../../../msserviceprofiler/modelevalstate"))
     return target_path
+
 
 class TestTrainCmd(TestCase):
     COMMAND_SUCCESS = 0
@@ -54,12 +56,13 @@ class TestTrainCmd(TestCase):
     def test_compare_ms_service_profiler_data(self):
         train_cmd = "cd " + self.MODELEVALSTATE_DIR + " && msserviceprofiler optimizer"
 
-        self.assertTrue(os.system(train_cmd) == self.COMMAND_SUCCESS)
+        self.assertEqual(os.system(train_cmd), self.COMMAND_SUCCESS)
         self.assertTrue(os.path.exists(self.RESULT_DIR))
 
-        matched_files = glob.glob(self.RESULT_DIR + "/store/" + "data_storage_*.csv")
+        pattern = os.path.join(self.RESULT_DIR, "store", "data_storage_*.csv")
+        matched_files = glob.glob(pattern)
 
-        self.assertTrue(len(matched_files) == 1)
+        self.assertEqual(len(matched_files), 1)
 
         csv_file = matched_files[0]  # 取第一个匹配的文件
         result = check_csv_no_empty_start(csv_file)  # 调用检查函数
