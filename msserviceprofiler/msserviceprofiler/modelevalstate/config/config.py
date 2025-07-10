ae9ffb1ceee200032c0cefc4e71e88760dd5c219
tucol <xujintao8@h-partners.com>
2025-07-10 19:23:59 +0800

    cleancode

--- msserviceprofiler/msserviceprofiler/modelevalstate/config/config.py
+++ msserviceprofiler/msserviceprofiler/modelevalstate/config/config.py
@@ -11,10 +11,10 @@ import numpy as np
 from loguru import logger
 from pydantic import BaseModel, field_validator, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource, JsonConfigSettingsSource
-from .base_config import INSTALL_PATH, RUN_PATH, ServiceType, custom_output, CUSTOM_OUTPUT, DeployPolicy, RUN_TIME
-from .base_config import modelevalstate_config_path, MODEL_EVAL_STATE_CONFIG_PATH, AnalyzeTool, BenchMarkPolicy
 from msserviceprofiler.modelevalstate.config.custom_command import BenchmarkCommandConfig, VllmBenchmarkCommandConfig
 from msserviceprofiler.modelevalstate.config.custom_command import MindieCommandConfig, VllmCommandConfig
+from .base_config import INSTALL_PATH, RUN_PATH, ServiceType, custom_output, CUSTOM_OUTPUT, DeployPolicy, RUN_TIME
+from .base_config import modelevalstate_config_path, MODEL_EVAL_STATE_CONFIG_PATH, AnalyzeTool, BenchMarkPolicy
 
 
 class OptimizerConfigField(BaseModel):
