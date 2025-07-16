fd90805e7a4744abf06502dfc17f6ff10b75bb02
王国伟 <leondgarse@163.com>
2025-07-16 03:23:16 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- msserviceprofiler/msserviceprofiler/vllm_profiler/vllm_v1/request_hookers.py
+++ msserviceprofiler/msserviceprofiler/vllm_profiler/vllm_v1/request_hookers.py
@@ -24,7 +24,30 @@ async def add_request_async(original_func, this, request_id, prompt, *args, **kw
     Profiler(Level.INFO).domain("Request").res(request_id).event("encode")
     return await original_func(this, request_id, prompt, *args, **kwargs)
 
+@vllm_hook(("vllm.engine.llm_engine", "LLMEngine._process_model_outputs"), min_version="0.8.4")
+def process_model_outputs(original_func, this, ctx, request_id=None, *args, **kwargs):
+    if len(ctx.output_queue) == 0:
+        return original_func(this, ctx, request_id, *args, **kwargs)
+    outputs, metadata_list, scheduler_outputs, _, _, _, skip = ctx.output_queue[0]
+    ret = original_func(this, ctx, request_id, *args, **kwargs)
 
+    request_id_list = []
+    for ii, (sch_seq_group, meta) in enumerate(zip(scheduler_outputs.scheduled_seq_groups, metadata_list)):
+        if ii in skip:
+            continue
+
+        seq_group, seq_request_id = sch_seq_group.seq_group, meta.request_id
+        request_id_list.append(seq_request_id)
+        if seq_group.is_finished() and len(seq_group.seqs) > 0:
+            cur_seq = seq_group.seqs[0]
+            profiler_recv = Profiler(Level.INFO).domain("Request").res(seq_request_id)
+            profiler_reply = Profiler(Level.INFO).domain("Request").res(seq_request_id)
+            profiler_recv.metric("recvTokenSize", cur_seq.get_prompt_len()).event("httpRes")
+            profiler_reply.metric("replyTokenSize", cur_seq.get_output_len()).event("httpRes")
+
+    prof = Profiler(Level.INFO).domain("Request").res(request_id_list)
+    prof.event("DecodeEnd")
+    return ret
 @vllm_hook(("vllm.v1.engine.output_processor", "OutputProcessor.process_outputs"), min_version="0.9.1")
 def process_outputs(original_func, this, engine_core_outputs, *args, **kwargs):
     if len(engine_core_outputs) == 0:
@@ -39,10 +62,11 @@ def process_outputs(original_func, this, engine_core_outputs, *args, **kwargs):
             recv_token_size = len(request_state.prompt_token_ids)
             reply_token_size = (request_state.stats.num_generation_tokens if request_state.stats else None)
             
+            profiler_recv = Profiler(Level.INFO).domain("Request").res(request_id)
+            profiler_reply = Profiler(Level.INFO).domain("Request").res(request_id)
+            profiler_recv.metric("recvTokenSize", recv_token_size).event("httpRes")
+            profiler_reply.metric("replyTokenSize", reply_token_size).event("httpRes")
 
-            profiler_recv_reply = Profiler(Level.INFO).domain("Request").res(request_id)
-            profiler_recv_reply.metric("recvTokenSize", recv_token_size).metric("replyTokenSize", reply_token_size)
-            profiler_recv_reply.event("httpRes")
     ret = original_func(this, engine_core_outputs, *args, **kwargs)
     prof = Profiler(Level.INFO).domain("Request").res(request_id_list)
     prof.event("DecodeEnd")
