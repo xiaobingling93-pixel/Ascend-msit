78327ee849fc3fc67cf3d33cd3283a8d901782bf
minghangc <29514143@qq.com>
2025-07-16 12:41:37 +0800

    fisrt init schedule patching

--- msserviceprofiler/msserviceprofiler/vllm_profiler/vllm_v1/batch_hookers.py
+++ msserviceprofiler/msserviceprofiler/vllm_profiler/vllm_v1/batch_hookers.py
@@ -11,9 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import threading
 from collections import Counter
 from ms_service_profiler import Profiler, Level
 from ..module_hook import vllm_hook
+
+class HookState:
+    def __init__(self):
+        self.request_id_to_prompt_token_len = {}
+        self.request_id_to_iter_size = {}
+
+
+# 线程本地存储
+_thread_local = threading.local()
+
+
+def _get_state() -> HookState:
+    """获取线程本地状态"""
+    if not hasattr(_thread_local, "hook_state"):
+        _thread_local.hook_state = HookState()
+    return _thread_local.hook_state
 
 
 @vllm_hook(("vllm.v1.engine.processor", "Processor.process_inputs"), min_version="0.9.1")
@@ -21,3 +38,29 @@ def process_inputs(original_func, this, request_id, *args, **kwargs):
     ret = original_func(this, request_id, *args, **kwargs)
     Profiler(Level.INFO).domain("BatchSchedule").res(request_id).metric_inc("WAITING", 1).event("ReqState")
     return ret
+
+@vllm_hook(("vllm.v1.sched.core.scheduler", "Scheduler.schedule"), min_version="0.9.1")
+def schedule(original_func, this, *args, **kwargs):
+    # from vllm.sequence import SequenceGroupMetadata
+    state = _get_state()
+    prof = Profiler(Level.INFO).domain("BatchSchedule").span_start("batchFrameworkProcessing")
+    scheduler_output = original_func(this, *args, **kwargs)
+
+    for scheduled_new_req in scheduler_output.scheduled_new_reqs:
+        state.request_id_to_prompt_token_len[scheduled_new_req.req_id] = len(scheduled_new_req.prompt_token_ids)
+
+    request_id_list, request_id_with_iter_list = [], []
+    for request_id, _ in scheduler_output.num_scheduled_tokens.items():
+        request_id_list.append({"rid": request_id})
+        iter_size = state.request_id_to_iter_size.get(request_id, -1) + 1  # start from 0
+        request_id_with_iter_list.append({"rid": request_id, "iter_size": iter_size})
+        state.request_id_to_iter_size[request_id] = iter_size
+
+        if request_id in scheduler_output.finished_req_ids:
+            state.request_id_to_prompt_token_len.pop(request_id, None)
+            state.request_id_to_iter_size.pop(request_id, None)
+
+    prof.res(request_id_with_iter_list)
+    prof.span_end()
+
+    return scheduler_output
