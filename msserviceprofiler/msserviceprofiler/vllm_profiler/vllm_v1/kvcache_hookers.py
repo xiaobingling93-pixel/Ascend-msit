de7318e4afdca038f89f677e742ae251d47444ff
王国伟 <leondgarse@163.com>
2025-07-17 07:54:06 +0000

    update
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- msserviceprofiler/msserviceprofiler/vllm_profiler/vllm_v1/kvcache_hookers.py
+++ msserviceprofiler/msserviceprofiler/vllm_profiler/vllm_v1/kvcache_hookers.py
@@ -23,3 +23,14 @@ def allocate_slots(original_func, this, request, *args, **kwargs):
     prof.metric("deviceBlock", num_blocks).event("Allocate")
     prof.domain("KVCache").res(request.request_id).metric("deviceBlock", num_blocks).event("blocks")
     return ret
+
+
+@vllm_hook(("vllm.v1.core.kv_cache_manager", "KVCacheManager.get_computed_blocks"), min_version="0.9.1")
+def get_computed_blocks(original_func, this, request, *args, **kwargs):
+    ret = original_func(this, request, *args, **kwargs)
+    if len(ret) > 1:
+        num_new_computed_tokens = ret[1]
+        cur_hit_rate = num_new_computed_tokens / request.num_tokens
+        prof = Profiler(Level.INFO).domain("KVCache").res(request.request_id)
+        prof.attr("hitCache", cur_hit_rate).event("GetCacheHitRate")
+    return ret
