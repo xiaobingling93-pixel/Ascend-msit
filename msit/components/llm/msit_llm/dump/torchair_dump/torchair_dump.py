a2f9585f83c749750f49d5dd426bc5e362dc1c35
jiangchao_j <chaojiang_j@163.com>
2025-08-18 19:49:44 +0800

    refine torchair dump and compare

--- msit/components/llm/msit_llm/dump/torchair_dump/torchair_dump.py
+++ msit/components/llm/msit_llm/dump/torchair_dump/torchair_dump.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2024 Huawei Technologies Co., Ltd.
+# Copyright (c) 2024-2025, Huawei Technologies Co., Ltd.
+# All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,12 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-import time
 
+import os
+
+from components.utils.file_utils import create_directory, check_path_readability, check_path_writability
+from msit_llm.common.constant import TORCHAIR_FX_DATA_DIRECTORY, TORCHAIR_GE_DATA_DIRECTORY
 from msit_llm.common.log import logger
-from msit_llm.common.utils import check_output_path_legality
-from components.utils.security_check import ms_makedirs
 
 
 def try_import_torchair():
@@ -24,23 +25,32 @@ def try_import_torchair():
         import torch
         import torch_npu
         import torchair
-    except ModuleNotFoundError as ee:
+    except ModuleNotFoundError as e:
         logger.error("torch or torch_npu with torchair not found. Try installing the latest torch_npu.")
-        raise ee
+        raise e
 
 
-def get_ge_dump_config(dump_path="msit_ge_dump", dump_mode="all", fusion_switch_file=None,
-                       dump_token=None, dump_layer=None):
+def get_ge_dump_config(
+        dump_path='',
+        dump_mode='all',
+        fusion_switch_file=None,
+        dump_token=None,
+        dump_layer=None,
+        compiler_config=None
+):
     try_import_torchair()
-
     from torchair.configs.compiler_config import CompilerConfig
 
-    config = CompilerConfig()
-    check_output_path_legality(dump_path)
-    dump_path = os.path.join(dump_path, "dump_" + time.strftime('%Y%m%d_%H%M%S'))  # Timestamp like '20240222_095519'
-    if not os.path.exists(dump_path):
-        ms_makedirs(dump_path, mode=0o750)
+    dump_path = os.path.realpath(os.path.join(dump_path, TORCHAIR_GE_DATA_DIRECTORY))
+    create_directory(dump_path)
+    check_path_readability(dump_path)
+    check_path_writability(dump_path)
+    if os.listdir(dump_path):
+        logger.warning('The specified directory for GE-Dump data is not empty, which may result in data mixing.')
 
+    if compiler_config is not None and not isinstance(compiler_config, CompilerConfig):
+        raise TypeError(f'compiler_config must be a CompilerConfig, but got {type(compiler_config)}')
+    config = compiler_config if compiler_config else CompilerConfig()
     # Generate GE mapping graph
     config.debug.graph_dump.type = "txt"
     if hasattr(config.debug.graph_dump, "_path"):  # interface changed since 8.0.RC1.b080
@@ -68,12 +78,23 @@ def get_ge_dump_config(dump_path="msit_ge_dump", dump_mode="all", fusion_switch_
     return config
 
 
-def get_fx_dump_config():
+def get_fx_dump_config(dump_path='', compiler_config=None):
     try_import_torchair()
-
     from torchair.configs.compiler_config import CompilerConfig
 
-    config = CompilerConfig()
+    dump_path = os.path.realpath(os.path.join(dump_path, TORCHAIR_FX_DATA_DIRECTORY))
+    create_directory(dump_path)
+    check_path_readability(dump_path)
+    check_path_writability(dump_path)
+    if os.listdir(dump_path):
+        logger.warning('The specified directory for FX-Dump data is not empty, which may result in data mixing.')
+
+    if compiler_config is not None and not isinstance(compiler_config, CompilerConfig):
+        raise TypeError(f'compiler_config must be a CompilerConfig, but got {type(compiler_config)}')
+    config = compiler_config if compiler_config else CompilerConfig()
     # Enable FX dump
     config.debug.data_dump.type = "npy"
+    if hasattr(config.debug.data_dump, "path"):
+        config.debug.data_dump.path = dump_path
+
     return config
