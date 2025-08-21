bec5dc35643b5100c3c5ecaf1b6013254e87701f
Anreywmh <18845895998@163.com>
2025-08-21 09:22:20 +0800

    【msModelSlim】[feature] V1 support iter-smooth

--- msmodelslim/msmodelslim/model/qwen.py
+++ msmodelslim/msmodelslim/model/qwen.py
@@ -3,21 +3,65 @@
 from typing import List
 
 from transformers import PreTrainedTokenizerBase
-
 from msmodelslim.model.adapter_types import AdapterConfig, MappingConfig
+from msmodelslim.quant.processor.kv_smooth import KVSmoothFusedInterface, \
+    KVSmoothFusedType, KVSmoothFusedUnit
 from msmodelslim.quant.processor.anti_outlier.smooth_interface import IterSmoothInterface
-from msmodelslim.utils.logging import logger_setter
+from msmodelslim.utils.exception import InvalidModelError
 from msmodelslim.utils.security.model import SafeGenerator
-
 from .default import DefaultModelAdapter
 from .factory import ModelFactory
+from ..utils.logging import logger_setter, get_logger
+
 
 @ModelFactory.register("Qwen2.5-7B-Instruct")
 @ModelFactory.register("Qwen2.5-32B-Instruct")
 @ModelFactory.register("Qwen2.5-72B-Instruct")
 @ModelFactory.register("Qwen2.5-Coder-7B-Instruct")
 @logger_setter(subfix='qwen2_5')
-class Qwen25ModelAdapter(DefaultModelAdapter):
+class Qwen25ModelAdapter(DefaultModelAdapter, KVSmoothFusedInterface):
+    def get_kvcache_smooth_fused_subgraph(self) -> List[KVSmoothFusedUnit]:
+        return [
+            KVSmoothFusedUnit(
+                attention_name=f"model.layers.{i}.self_attn",
+                layer_idx=i,
+                fused_from_query_states_name="q_proj",
+                fused_from_key_states_name="k_proj",
+                fused_type=KVSmoothFusedType.StateViaRopeToLinear
+            )
+            for i in range(self.config.num_hidden_layers)
+        ]
+
+    def get_head_dim(self) -> int:
+        if not hasattr(self.config, 'hidden_size'):
+            raise InvalidModelError("hidden_size is not found in config.json",
+                                    action="Please check the model config.json")
+        if not hasattr(self.config, 'num_attention_heads'):
+            raise InvalidModelError("num_attention_heads is not found in config.json",
+                                    action="Please check the model config.json")
+        if self.config.num_attention_heads == 0:
+            raise InvalidModelError("num_attention_heads is 0 in config.json, which should be greater than 0",
+                                    action="Please check the model config.json")
+        return self.config.hidden_size // self.config.num_attention_heads
+
+    def get_num_key_value_groups(self) -> int:
+        if not hasattr(self.config, 'num_attention_heads'):
+            raise InvalidModelError("num_attention_heads is not found in config.json",
+                                    action="Please check the model config.json")
+        if not hasattr(self.config, 'num_key_value_heads'):
+            raise InvalidModelError("num_key_value_heads is not found in config.json",
+                                    action="Please check the model config.json")
+        if self.config.num_key_value_heads == 0:
+            raise InvalidModelError("num_key_value_heads is 0 in config.json, which should be greater than 0",
+                                    action="Please check the model config.json")
+        return self.config.num_attention_heads // self.config.num_key_value_heads
+
+    def get_num_key_value_heads(self) -> int:
+        if not hasattr(self.config, 'num_key_value_heads'):
+            raise InvalidModelError("num_key_value_heads is not found in config.json",
+                                    action="Please check the model config.json")
+        return self.config.num_key_value_heads
+
     def _get_model_pedigree(self) -> str:
         return 'qwen2_5'
 
@@ -31,12 +75,57 @@ class Qwen25ModelAdapter(DefaultModelAdapter):
             eos_token='<|endoftext|>',
             trust_remote_code=trust_remote_code)
 
-
 @ModelFactory.register("Qwen3-8B")
 @ModelFactory.register("Qwen3-14B")
 @ModelFactory.register("Qwen3-32B")
 @logger_setter(subfix='qwen3')
 class Qwen3ModelAdapter(DefaultModelAdapter, IterSmoothInterface):
+    def get_kvcache_smooth_fused_subgraph(self) -> List[KVSmoothFusedUnit]:
+        return [
+            KVSmoothFusedUnit(
+                attention_name=f"model.layers.{i}.self_attn",
+                layer_idx=i,
+                fused_from_query_states_name="q_norm",
+                fused_from_key_states_name="k_norm",
+                fused_type=KVSmoothFusedType.StateViaRopeToNorm
+            )
+            for i in range(self.config.num_hidden_layers)
+        ]
+
+    def get_head_dim(self) -> int:
+        if hasattr(self.config, 'head_dim'):
+            return self.config.head_dim
+
+        get_logger().warning(f'head_dim is not found in config.json, use hidden_size // num_attention_heads instead')
+        if not hasattr(self.config, 'hidden_size'):
+            raise InvalidModelError("hidden_size is not found in config.json",
+                                    action="Please check the model config.json")
+        if not hasattr(self.config, 'num_attention_heads'):
+            raise InvalidModelError("num_attention_heads is not found in config.json",
+                                    action="Please check the model config.json")
+        if self.config.num_attention_heads == 0:
+            raise InvalidModelError("num_attention_heads is 0 in config.json, which should be greater than 0",
+                                    action="Please check the model config.json")
+        return self.config.hidden_size // self.config.num_attention_heads
+
+    def get_num_key_value_groups(self) -> int:
+        if not hasattr(self.config, 'num_attention_heads'):
+            raise InvalidModelError("num_attention_heads is not found in config.json",
+                                    action=f"Please check config.json in {self.ori}")
+        if not hasattr(self.config, 'num_key_value_heads'):
+            raise InvalidModelError("num_key_value_heads is not found in config.json",
+                                    action=f"Please check config.json in {self.ori}")
+        if self.config.num_key_value_heads == 0:
+            raise InvalidModelError("num_key_value_heads is 0 in config.json, which should be greater than 0",
+                                    action=f"Please check config.json in {self.ori}")
+        return self.config.num_attention_heads // self.config.num_key_value_heads
+
+    def get_num_key_value_heads(self) -> int:
+        if not hasattr(self.config, 'num_key_value_heads'):
+            raise InvalidModelError("num_key_value_heads is not found in config.json",
+                                    action=f"Please check config.json in {self.ori}")
+        return self.config.num_key_value_heads
+
     def get_adapter_config_for_subgraph(self) -> List[AdapterConfig]:
         adapter_config = []
         for layer_idx in range(self.config.num_hidden_layers):
