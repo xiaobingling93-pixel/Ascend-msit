5d353b4af645e3a1fd57c62780021508f009ba72
AvadaKedavrua <anonymousdev@163.com>
2025-09-05 14:59:40 +0800

    bug fixc

--- msprechecker/msprechecker/checkers/config/model_config.py
+++ msprechecker/msprechecker/checkers/config/model_config.py
@@ -25,27 +25,34 @@ class ModelConfigChecker(BaseChecker):
     def _check(self, results):
         cur_transformers_version = get_pkg_version('transformers')
 
-        if results['torch_dtype'] != 'float16':
+        torch_dtype = results.get('torch_dtype', '<missing>')
+        if torch_dtype != 'float16':
             self.error_handler.add_error(
                 path="torch_dtype",
-                actual=results['torch_dtype'],
+                actual=torch_dtype,
                 expected='float16',
                 reason='部分模型的算子可能不支持 bfloat16, 请确保当前模型算子支持 bfloat16',
                 severity='low'
             )
-        if not cur_transformers_version or results['transformers_version'] > cur_transformers_version:
+
+        transformers_version = results.get('transformers_version')
+        if transformers_version and (
+            not cur_transformers_version or transformers_version > cur_transformers_version 
+        ):
             self.error_handler.add_error(
                 path="transformers_version",
-                actual=results['transformers_version'],
+                actual=transformers_version,
                 expected=cur_transformers_version,
                 reason=f'当前机器的 "transformers" 的版本（{cur_transformers_version}）如果小于配置文件要求版本，会导致服务启动失败',
                 severity='medium'
             )
-        if results['model_type'].startswith('deepseek') and \
+
+        model_type = results.get('model_type', "<missing>")
+        if model_type.startswith('deepseek') and \
            'deepseek_' not in results['model_type'] and self.rule_manager.framework == "vllm":
             self.error_handler.add_error(
                 path="model_type",
-                actual=results['model_type'],
+                actual=model_type,
                 expected="deepseek_xxxx",
                 reason=f'vllm 框架下, DeepSeek 系列模型的 "model_type" 需要添加下划线，如："deepseek_v2"，否则会导致服务部署失败',
                 severity='high'
--- msprechecker/msprechecker_env.sh
+++ msprechecker/msprechecker_env.sh
@@ -7,9 +7,35 @@
 # --------------------------------------------------
 
 if [ "$1" = "0" ]; then
+    export MIES_CONTAINER_IP="<missing>"
+    export PYTORCH_NPU_ALLOC_CONF="<missing>"
+    export ATB_WORKSPACE_MEM_ALLOC_ALG_TYPE="<missing>"
+    export ATB_WORKSPACE_MEM_ALLOC_GLOBAL="<missing>"
+    export HCCL_OP_EXPANSION_MODE="<missing>"
+    export NPU_MEMORY_FRACTION="<missing>"
+    export ATB_LLM_HCCL_ENABLE="<missing>"
+    export ATB_LAYER_INTERNAL_TENSOR_REUSE="<missing>"
+    export HCCL_CONNECT_TIMEOUT="<missing>"
+    export HCCL_EXEC_TIMEOUT="<missing>"
+    export ATB_LLM_ENABLE_AUTO_TRANSPOSE="<missing>"
+    export MINDIE_ASYNC_SCHEDULING_ENABLE="<missing>"
     export MINDIE_LOG_TO_FILE="<missing>"
     export MINDIE_LOG_TO_STDOUT="<missing>"
+    export OMP_NUM_THREADS="<missing>"
 else
+    export MIES_CONTAINER_IP="172.18.50.239" # MIES_CONTAINER_IP表示当前容器IP地址，建议设置为当前容器IP
+    export PYTORCH_NPU_ALLOC_CONF="expandable_segments:True" # 需要开启torch_npu虚拟内存机制
+    export ATB_WORKSPACE_MEM_ALLOC_ALG_TYPE="3" # workspace内存分配算法选择，建议设置为3，最大优化显存碎片与workspace空间
+    export ATB_WORKSPACE_MEM_ALLOC_GLOBAL="1" # 建议开启全局中间tensor内存分配算法，提升显存利用率
+    export HCCL_OP_EXPANSION_MODE="AIV" # HCCL_OP_EXPANSION_MODE建议设置为AIV，设置通信算法的编排展开位置在Device侧的AI Vector Core计算单元
+    export NPU_MEMORY_FRACTION="0.96" # A3单机DeepSeek场景下，NPU显存比建议设置为0.96，可根据实际业务场景加大，出现out of memory时可以尝试调大该值
+    export ATB_LLM_HCCL_ENABLE="1" # 需要开启HCCL通信后端
+    export ATB_LAYER_INTERNAL_TENSOR_REUSE="1" # 需要开启复用Layer间的中间Tensor复用
+    export HCCL_CONNECT_TIMEOUT="7200" # HCCL建链超时时间建议设置为7200秒
+    export HCCL_EXEC_TIMEOUT="0" # HCCL执行超时时间建议设置为0，不限制超时
+    export ATB_LLM_ENABLE_AUTO_TRANSPOSE="0" # 不能开启权重右矩阵自动转置
+    export MINDIE_ASYNC_SCHEDULING_ENABLE="1" # 建议开启MindIE异步调度特性，提升推理性能
     export MINDIE_LOG_TO_FILE="1" # 建议将MindIE日志写入文件，便于排查问题
     export MINDIE_LOG_TO_STDOUT="1" # 建议将MindIE日志打屏，便于通过查看k8s日志查看程序运行状态
+    export OMP_NUM_THREADS="16" # OpenMP并行数建议设置为16
 fi
