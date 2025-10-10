63cbae863eacc386960f5a4a3958b667d91f4e46
xyxin_006 <xyxin_hit@163.com>
2025-10-10 17:12:01 +0800

    【msmodelslim】【UT】补充quarot和autoround UT
    Co-authored-by: xyxin_006<xyxin_hit@163.com>

---
+++ msmodelslim/test/resources/fake/qwen3_dense/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+
+__all__ = ["FakeQwen3Creator", "FakeQwen3Model"]
+
+from .fake import FakeQwen3Creator, FakeQwen3Model
+
+
