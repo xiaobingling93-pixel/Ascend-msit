66759deaefdf47e5d68494725ccd074ffbe0b7a5
ylw1234 <lwying007@126.com>
2025-12-11 10:36:11 +0800

    【bugfix】aclruntime and aisbench sha256 update
    Co-authored-by: ylw1234<lwying007@126.com>

--- msit/components/benchmark/msit_benchmark/install.sh
+++ msit/components/benchmark/msit_benchmark/install.sh
@@ -52,26 +52,30 @@ download_and_install_aclruntime() {
 
     if [ "$PLATFORM" == "aarch64" ]; then
         if [ "$PYTHON3_MINI_VERSION" == "7" ]; then
-            sha256Value="f1b6da04cb454bdf1f3f3373c346ec485dca7c425184806faa6b9f197e82016b"
+            sha256Value="2a0e847d382e7afb72819dc9ae287aaaebca0d168a8767a4b10d984f3c451c91"
         elif [ "$PYTHON3_MINI_VERSION" == "8" ]; then
-            sha256Value="b7a15ac9fbb94f7f52e6cf574db4eb63101939a84c118e4c72dfd2bdf4f0c39a"
+            sha256Value="903f27f1e44d14d2a17cd6f00784562bee90fcfd467beaca014ba8121b6059a8"
         elif [ "$PYTHON3_MINI_VERSION" == "9" ]; then
-            sha256Value="af3be0a0fb0c74dabb0c3e7307dd2054673f635d22958011fa860f71a42d5dd4"
+            sha256Value="fe5e28e3acf08d457953962cc3a8208a500fdc648c710fbc156c3e83bdd7fa4a"
         elif [ "$PYTHON3_MINI_VERSION" == "10" ]; then
-            sha256Value="2d7298ca9b0cf62c9914772f0d25c1326abef777bc451b3fdc345b0896835011"
+            sha256Value="206c2c0e9caa465437465ef2a277bc7f4f3b283cba762cf6ea57d5a7d80aff1e"
+        elif [ "$PYTHON3_MINI_VERSION" == "11" ]; then
+            sha256Value="a219b54ac9cf46a2cd925f7250339268cab119859beb2cfd4d1fef123eee6378"
         else
             echo "Unsupported python3 version"
             exit 1
         fi
     elif [ "$PLATFORM" == "x86_64" ]; then
         if [ "$PYTHON3_MINI_VERSION" == "7" ]; then
-            sha256Value="a4be8a768e227f8b2db8f8b73a00e1541786cc9c42c726a5809f0a085bfd9168"
+            sha256Value="2cfe9f1f7df767c42d8d2aaff244e930ce2c4dc28eca7be1d2b07376bf4b533d"
         elif [ "$PYTHON3_MINI_VERSION" == "8" ]; then
-            sha256Value="c25e152364d3be7bff473162f8928547c3534f1826a35b867043372a58f0a380"
+            sha256Value="0189755441d65b550f0e898c37d3a7eb451ed7c91b560cdb7786acc15b666eb8"
         elif [ "$PYTHON3_MINI_VERSION" == "9" ]; then
-            sha256Value="9768354e32452c0073800064985f6b5169f7d7821452b008e3d95187162931b6"
+            sha256Value="c518c671bf9c2f5a738dd58cb13c7573828d7e98576ed2201dfb2969205f7ddf"
         elif [ "$PYTHON3_MINI_VERSION" == "10" ]; then
-            sha256Value="45d781faff585a92b58b7a6b4bd0564fc521bd0b578e31de6f852a19f069e664"
+            sha256Value="3a8e4162c8d6f0e7fcdfd1e4ae100ae9594eaf0ad1e68e7efd4437cf241a2aab"
+        elif [ "$PYTHON3_MINI_VERSION" == "11" ]; then
+            sha256Value="0f99823b7a76d621e2cefa59d19e8fc36f3f5f763bb6e9cc0922a96ba34d125e"
         else
             echo "Unsupported python3 version"
             exit 1
@@ -113,7 +117,7 @@ download_and_install_ais_bench() {
         wget -c "${WHL_BASE_URL}${WHL_NAME}"
     fi
 
-    sha256Value="ff55373a11d9975eaad497a230c9fb0d93856dc184790b8f168143c9c5f1cccd"
+    sha256Value="1d533fc6e48ef0322a163a3294fb640f447a7d461ccb4c840623f9f93082f997"
     sha256Data=$(sha256sum "$WHL_NAME" | cut -d' ' -f1)
     if [[ "${sha256Data}" != "${sha256Value}" ]]; then
         echo "Failed to verify sha256: $WHL_NAME"
